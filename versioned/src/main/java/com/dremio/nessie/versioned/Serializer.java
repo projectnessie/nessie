5a5f88797239e3331d9ad9d932cc4c50a90e2c7f
Jacques Nadeau <jacques@apache.org>
2020-08-11 08:28:46 -0700

    Add assets interface

--- versioned/src/main/java/com/dremio/nessie/versioned/Serializer.java
+++ versioned/src/main/java/com/dremio/nessie/versioned/Serializer.java
@@ -16,6 +16,7 @@
 package com.dremio.nessie.versioned;
 
 import java.nio.ByteBuffer;
+import java.util.stream.Stream;
 
 /**
  * Used to serialize & deserialize the values in the store. Provided to an implementation of VersionStore on construction.
@@ -25,4 +26,26 @@ public interface Serializer<V> {
   ByteBuffer toBytes(V value);
 
   V fromBytes(ByteBuffer bytes);
+
+  Stream<Asset> getAssets(V value);
+
+  /**
+   * An asset is an external object that is mapped to a value. As part of garbage collection, assets will be deleted as
+   * necessary to ensure assets are not existing if their references in Nessie are removed.
+   */
+  interface Asset extends Comparable<Asset> {
+
+    /**
+     * An idempotent deletion. Given the lack of consistency guarantees in deletion of external assets, Nessie may call this method multiple times on equivalen assets.
+     */
+    void delete();
+
+    @Override
+    boolean equals(Object other);
+
+    @Override
+    int hashCode();
+
+
+  }
 }
