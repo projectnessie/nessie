289f10480bd79478e1d430afcc03a7ad7af4a361
Laurent Goujon <laurent@dremio.com>
2020-08-18 11:08:02 -0700

    Fixup for Hash

--- versioned/spi/src/main/java/com/dremio/nessie/versioned/Hash.java
+++ versioned/spi/src/main/java/com/dremio/nessie/versioned/Hash.java
@@ -15,9 +15,20 @@
  */
 package com.dremio.nessie.versioned;
 
-import java.util.Arrays;
+import static java.util.Objects.requireNonNull;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.StringReader;
+import java.io.StringWriter;
+import java.util.Objects;
+
+import javax.annotation.Nonnull;
 
 import com.google.common.io.BaseEncoding;
+import com.google.common.io.BaseEncoding.DecodingException;
+import com.google.protobuf.ByteString;
 
 /**
  * Describes a specific point in time/history. Internally is a binary value but a string representation is available.
@@ -25,11 +36,10 @@ import com.google.common.io.BaseEncoding;
 public final class Hash implements Ref {
 
   private static final BaseEncoding ENCODING = BaseEncoding.base16().lowerCase();
+  private final ByteString bytes;
 
-  private final byte[] bytes;
-
-  private Hash(byte[] bytes) {
-    this.bytes = bytes;
+  private Hash(ByteString bytes) {
+    this.bytes = requireNonNull(bytes);
   }
 
   /**
@@ -39,23 +49,62 @@ public final class Hash implements Ref {
    * @return
    */
   public String asString() {
-    return ENCODING.encode(bytes);
+    final int maxSize = 2 * bytes.size();
+    try (final StringWriter sw = new StringWriter(maxSize);
+        final OutputStream os = ENCODING.encodingStream(sw)) {
+      bytes.writeTo(os);
+      return sw.toString();
+    } catch (IOException e) {
+      // Should not happen as all operations are in memory
+      throw new AssertionError(e);
+    }
+  }
+
+  /**
+   * Gets the bytes representation of the hash.
+   * @return the hash's bytes
+   */
+  public ByteString asBytes() {
+    return bytes;
   }
 
   /**
    * Creates a hash instance from its string representation.
    *
    * @param hash the string representation of the hash
-   * @return
+   * @return a {@code Hash} instance
+   * @throws IllegalArgumentException if {@code hash} is not a valid representation of a hash
+   * @throws NullPointerException if {@code hash} is {@code null}
    */
-  public static Hash of(String hash) {
-    byte[] bytes = ENCODING.decode(hash);
+  public static Hash of(@Nonnull String hash) {
+    requireNonNull(hash);
+    final int maxSize = hash.length() / 2;
+    try (final StringReader sr = new StringReader(hash);
+        final InputStream is = ENCODING.decodingStream(sr)) {
+      ByteString bytes = ByteString.readFrom(is, maxSize);
+      return Hash.of(bytes);
+    } catch (DecodingException e) {
+      throw new IllegalArgumentException(e);
+    } catch (IOException e) {
+      // Should not happen as all operations are in memory
+      throw new AssertionError(e);
+    }
+  }
+
+  /**
+   * Creates a hash instance from its bytes' representation.
+   *
+   * @param bytes the bytes' representation of the hash
+   * @return a {@code Hash} instance
+   * @throws NullPointerException if {@code hash} is {@code null}
+   */
+  public static Hash of(@Nonnull ByteString bytes) {
     return new Hash(bytes);
   }
 
   @Override
   public int hashCode() {
-    return Arrays.hashCode(bytes);
+    return bytes.hashCode();
   }
 
   @Override
@@ -64,7 +113,7 @@ public final class Hash implements Ref {
       return false;
     }
     Hash that = (Hash) obj;
-    return Arrays.equals(this.bytes, that.bytes);
+    return Objects.equals(this.bytes, that.bytes);
   }
 
   @Override
