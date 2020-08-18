eaed70306de625348a31423ce2102e46a2bf3fea
Laurent Goujon <laurentgo@users.noreply.github.com>
2020-08-18 11:57:34 -0700

    Add checked exceptions to VersionStore (#31)
    * Create a set of checked exception for the VersionStore API and modify
    the interface to use those.
    * Fixup for Hash
    * Cleanup versioned spi pom

--- versioned/spi/src/main/java/com/dremio/nessie/versioned/Hash.java
+++ versioned/spi/src/main/java/com/dremio/nessie/versioned/Hash.java
@@ -15,9 +15,20 @@
  */
 package com.dremio.nessie.versioned;
 
+import static java.util.Objects.requireNonNull;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.StringReader;
+import java.io.StringWriter;
+import java.util.Objects;
+
+import javax.annotation.Nonnull;
+
 import com.google.common.io.BaseEncoding;
+import com.google.common.io.BaseEncoding.DecodingException;
 import com.google.protobuf.ByteString;
-import com.google.protobuf.UnsafeByteOperations;
 
 /**
  * Describes a specific point in time/history. Internally is a binary value but a string representation is available.
@@ -25,20 +36,84 @@ import com.google.protobuf.UnsafeByteOperations;
 public final class Hash implements Ref {
 
   private static final BaseEncoding ENCODING = BaseEncoding.base16().lowerCase();
-
   private final ByteString bytes;
 
   private Hash(ByteString bytes) {
-    this.bytes = bytes;
+    this.bytes = requireNonNull(bytes);
   }
 
+  /**
+   * Generates a string representation of the hash suitable to be used with
+   * {@link #of(String)}.
+   *
+   * @return
+   */
   public String asString() {
-    return ENCODING.encode(bytes.toByteArray());
+    final int maxSize = 2 * bytes.size();
+    try (final StringWriter sw = new StringWriter(maxSize);
+        final OutputStream os = ENCODING.encodingStream(sw)) {
+      bytes.writeTo(os);
+      return sw.toString();
+    } catch (IOException e) {
+      // Should not happen as all operations are in memory
+      throw new AssertionError(e);
+    }
   }
 
-  public static Hash of(String hash) {
-    byte[] bytes = ENCODING.decode(hash);
-    return new Hash(UnsafeByteOperations.unsafeWrap(bytes));
+  /**
+   * Gets the bytes representation of the hash.
+   * @return the hash's bytes
+   */
+  public ByteString asBytes() {
+    return bytes;
+  }
+
+  /**
+   * Creates a hash instance from its string representation.
+   *
+   * @param hash the string representation of the hash
+   * @return a {@code Hash} instance
+   * @throws IllegalArgumentException if {@code hash} is not a valid representation of a hash
+   * @throws NullPointerException if {@code hash} is {@code null}
+   */
+  public static Hash of(@Nonnull String hash) {
+    requireNonNull(hash);
+    final int maxSize = hash.length() / 2;
+    try (final StringReader sr = new StringReader(hash);
+        final InputStream is = ENCODING.decodingStream(sr)) {
+      ByteString bytes = ByteString.readFrom(is, maxSize);
+      return Hash.of(bytes);
+    } catch (DecodingException e) {
+      throw new IllegalArgumentException(e);
+    } catch (IOException e) {
+      // Should not happen as all operations are in memory
+      throw new AssertionError(e);
+    }
+  }
+
+  /**
+   * Creates a hash instance from its bytes' representation.
+   *
+   * @param bytes the bytes' representation of the hash
+   * @return a {@code Hash} instance
+   * @throws NullPointerException if {@code hash} is {@code null}
+   */
+  public static Hash of(@Nonnull ByteString bytes) {
+    return new Hash(bytes);
+  }
+
+  @Override
+  public int hashCode() {
+    return bytes.hashCode();
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (!(obj instanceof Hash)) {
+      return false;
+    }
+    Hash that = (Hash) obj;
+    return Objects.equals(this.bytes, that.bytes);
   }
 
   @Override
---
+++ versioned/spi/src/main/java/com/dremio/nessie/versioned/ReferenceConflictException.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright (C) 2020 Dremio
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.dremio.nessie.versioned;
+
+import static java.lang.String.format;
+
+import java.util.Optional;
+
+import javax.annotation.Nonnull;
+
+/**
+ * Exception thrown when the hash associated with a named reference does not match with the hash provided by the caller.
+ */
+public class ReferenceConflictException extends VersionStoreException {
+  private static final long serialVersionUID = 4381980193289615523L;
+
+  public ReferenceConflictException(String message) {
+    super(message);
+  }
+
+  /**
+   * Create a {@code ReferenceConflictException} instance with an accurate message
+   * based on the provided named referenced and the compared hashes.
+   *
+   * @param ref the named reference
+   * @param expected the hash expected to be found in the store
+   * @param actual the hash found in the store for the reference
+   * @return a {@code ReferenceNotFoundException} instance
+   * @throws NullPointerException if {@code ref} is {@code null}.
+   */
+  @Nonnull
+  public static ReferenceConflictException forReference(@Nonnull NamedRef ref, @Nonnull Optional<Hash> expected,
+      @Nonnull Optional<Hash> actual) {
+    final String expectedArgument = expected.map(Hash::asString).orElse("no reference");
+    final String actualArgument = actual.map(Hash::asString).orElse("no reference");
+    final String refType;
+    if (ref instanceof BranchName) {
+      refType = "branch";
+    } else if (ref instanceof TagName) {
+      refType = "tag";
+    } else {
+      refType = "named ref";
+    }
+    return new ReferenceConflictException(format("Expected %s for %s '%s' but was %s",
+        expectedArgument, refType, ref.getName(), actualArgument));
+  }
+}
