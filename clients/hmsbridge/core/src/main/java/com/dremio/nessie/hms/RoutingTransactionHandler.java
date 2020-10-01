b17c0b8d0f2cddd76733d25e47469c8b1fdc25d4
Jacques Nadeau <jacques@apache.org>
2020-09-30 19:17:48 -0700

    Add HMS Bridge Capability (#106)
    * Implement the Hive Metastore Bridge

---
+++ clients/hmsbridge/core/src/main/java/com/dremio/nessie/hms/RoutingTransactionHandler.java
@@ -0,0 +1,81 @@
+/*
+ * Copyright (C) 2020 Dremio
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.dremio.nessie.hms;
+
+import java.util.Optional;
+import java.util.function.Supplier;
+
+import org.apache.hadoop.hive.metastore.RawStore;
+
+class RoutingTransactionHandler implements TransactionHandler {
+
+  Supplier<NessieStore> nessie;
+  Supplier<Optional<RawStore>> delegate;
+
+  RoutingTransactionHandler(Supplier<NessieStore> nessie, Supplier<Optional<RawStore>> delegate) {
+    super();
+    this.nessie = nessie;
+    this.delegate = delegate;
+  }
+
+  public void routedDelegate() {
+
+  }
+
+  public void routedNessie() {
+
+  }
+
+  private boolean hasDelegate() {
+    return delegate.get().isPresent();
+  }
+
+  @Override
+  public boolean isActiveTransaction() {
+    boolean isActive = nessie.get().isActiveTransaction();
+
+    if (!hasDelegate()) {
+      return isActive;
+    }
+
+    return delegate.get().get().isActiveTransaction() && isActive;
+  }
+
+  @Override
+  public void rollbackTransaction() {
+  }
+
+  @Override
+  public boolean openTransaction() {
+    boolean ret = nessie.get().openTransaction();
+    delegate.get().ifPresent(r -> r.openTransaction());
+    return ret;
+  }
+
+  @Override
+  public boolean commitTransaction() {
+    boolean ret = nessie.get().commitTransaction();
+    delegate.get().ifPresent(r -> r.commitTransaction());
+    return ret;
+  }
+
+  @Override
+  public void shutdown() {
+    nessie.get().shutdown();
+    delegate.get().ifPresent(r -> r.shutdown());
+  }
+
+}
---
+++ clients/hmsbridge/hive2/src/test/java/com/dremio/nessie/hms/ITTestHive2TableOperations.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (C) 2020 Dremio
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.dremio.nessie.hms;
+
+import java.util.Map;
+import java.util.function.Function;
+
+import org.apache.hadoop.hive.conf.HiveConf;
+import org.junit.jupiter.api.BeforeAll;
+import org.projectnessie.Hive2NessieRawStore;
+
+import com.dremio.nessie.client.NessieClient;
+import com.google.common.collect.ImmutableMap;
+import com.klarna.hiverunner.annotations.HiveProperties;
+
+public class ITTestHive2TableOperations extends BaseTableOperations {
+
+  @HiveProperties
+  public static final Map<String, String> properties = ImmutableMap.<String, String>builder()
+      .put(HiveConf.ConfVars.METASTORE_RAW_STORE_IMPL.varname, Hive2NessieRawStore.class.getName())
+      .put("hive.exec.dynamic.partition.mode","nonstrict")
+      .put(NessieClient.CONF_NESSIE_URL, URL)
+      .build();
+
+  @BeforeAll
+  static void setupNessieClient() {
+    client = NessieClient.withConfig(properties::get);
+  }
+
+  @Override
+  protected Function<String, String> configFunction() {
+    return properties::get;
+  }
+
+}
