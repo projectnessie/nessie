5c8d430e12ab2e3397ab5f12515c560f33ae5d15
Laurent Goujon <laurentgo@users.noreply.github.com>
2020-10-02 19:51:16 -0700

    Automate deployment to OSSRH/Docker Hub (#258)
    * Add Javadoc and Source mojos for release profile
    
    Update release profile to generate source and javadoc jars for the
    project.
    
    * Change distribution management to OSSRH
    
    * Add maven-gpg-plugin
    
    Add maven-gpg-plugin:sign goal to the release profile
    
    * Add extra project information
    
    * Fix spark3 dependencies
    
    * Add missing description
    
    * Switch to OSSRH/Docker Hub for packages
    
    * fixing pr maven cache
    
    * Add javadoc execution to pull requests

--- clients/client/src/main/java/com/dremio/nessie/client/rest/ObjectMapperContextResolver.java
+++ clients/client/src/main/java/com/dremio/nessie/client/rest/ObjectMapperContextResolver.java
@@ -23,7 +23,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 
 /**
- * custom resolver for ObjectMapper to enable optional settings on json <-> object conversion.
+ * custom resolver for ObjectMapper to enable optional settings on json &lt;-&gt; object conversion.
  */
 @Provider
 public class ObjectMapperContextResolver implements ContextResolver<ObjectMapper> {
