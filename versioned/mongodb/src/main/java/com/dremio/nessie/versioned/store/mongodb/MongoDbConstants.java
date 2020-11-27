5beac558291d32c45fea95c1810f2cb7aac2c964
Steve Lord <steve.lord@dremio.com>
2020-11-27 12:12:29 -0800

    Updated Mongo collection names to be more meaningful.

--- versioned/mongodb/src/main/java/com/dremio/nessie/versioned/store/mongodb/MongoDbConstants.java
+++ versioned/mongodb/src/main/java/com/dremio/nessie/versioned/store/mongodb/MongoDbConstants.java
@@ -21,13 +21,13 @@ package com.dremio.nessie.versioned.store.mongodb;
 public class MongoDbConstants {
   // The names of the collections in the database. These relate to
   // {@link com.dremio.nessie.versioned.store.ValueType}
-  public static final String L1_COLLECTION = "l1";
-  public static final String L2_COLLECTION = "l2";
-  public static final String L3_COLLECTION = "l3";
-  public static final String REF_COLLECTION = "r";
-  public static final String VALUE_COLLECTION = "v";
-  public static final String KEY_FRAGMENT_COLLECTION = "k";
-  public static final String COMMIT_METADATA_COLLECTION = "m";
+  public static final String L1_COLLECTION = "nessie_l1";
+  public static final String L2_COLLECTION = "nessie_l2";
+  public static final String L3_COLLECTION = "nessie_l3";
+  public static final String REF_COLLECTION = "nessie_ref";
+  public static final String VALUE_COLLECTION = "nessie_value";
+  public static final String KEY_FRAGMENT_COLLECTION = "nessie_key_fragment";
+  public static final String COMMIT_METADATA_COLLECTION = "nessie_commit_metadata";
 
   public static final String STRING_PREFIX = "s";
   public static final String NUMBER_PREFIX = "n";
