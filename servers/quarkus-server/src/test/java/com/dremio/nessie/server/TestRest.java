1ed9a65f61449b10f01a4e7053ff9714b217399c
Robert Stupp <snazy@snazy.de>
2020-12-16 16:53:39 +0100

    Fix handling of tag/branch names in the REST API paths containging characters that need to be escaped.
    Characters like `%`, `/`, etc need to be URI-escaped in the HTTP request URI to work properly.
    
    The fix is rather simple: just use `javax.ws.rs.client.WebTarget#resolveTemplate(java.lang.String, java.lang.Object, boolean)` where necessary.
    
    We probably also need some functionality to check whether a tag/branch names contains valid characters to prevent that "bad" characters (e.g. control characters) get into the systems.
    
    Fixes #395

--- servers/quarkus-server/src/test/java/com/dremio/nessie/server/TestRest.java
+++ servers/quarkus-server/src/test/java/com/dremio/nessie/server/TestRest.java
@@ -17,6 +17,9 @@
 package com.dremio.nessie.server;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.instanceOf;
+import static org.hamcrest.Matchers.notNullValue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
@@ -26,6 +29,8 @@ import java.util.List;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import com.dremio.nessie.api.ContentsApi;
 import com.dremio.nessie.api.TreeApi;
@@ -35,10 +40,17 @@ import com.dremio.nessie.error.NessieConflictException;
 import com.dremio.nessie.error.NessieNotFoundException;
 import com.dremio.nessie.model.Branch;
 import com.dremio.nessie.model.ContentsKey;
+import com.dremio.nessie.model.EntriesResponse;
 import com.dremio.nessie.model.IcebergTable;
+import com.dremio.nessie.model.ImmutableMerge;
+import com.dremio.nessie.model.ImmutableOperations;
+import com.dremio.nessie.model.ImmutablePut;
+import com.dremio.nessie.model.LogResponse;
 import com.dremio.nessie.model.MultiGetContentsRequest;
 import com.dremio.nessie.model.MultiGetContentsResponse.ContentsWithKey;
+import com.dremio.nessie.model.Operations;
 import com.dremio.nessie.model.Reference;
+import com.dremio.nessie.model.Tag;
 
 import io.quarkus.test.junit.QuarkusTest;
 
@@ -55,6 +67,95 @@ class TestRest {
     this.client = new NessieClient(AuthType.NONE, path, null, null);
     tree = client.getTreeApi();
     contents = client.getContentsApi();
+  }
+
+  @ParameterizedTest
+  @ValueSource(strings = {
+      "normal",
+      "with space",
+      "with%sign",
+      "slash/thing",
+      "all the/things%:*\u00E4\u00F6\u00FC" // some german umlauts as well
+  })
+  void referenceNames(String refNamePart) throws NessieNotFoundException, NessieConflictException {
+    String tagName = "tag" + refNamePart;
+    String branchName = "branch" + refNamePart;
+    String branchName2 = "branch2" + refNamePart;
+
+    String tagHash = null;
+    String branchHash;
+    String branchHash2;
+    String newHash = null;
+
+    try {
+      String someHash = tree.getReferenceByName("main").getHash();
+
+      tree.createReference(Tag.of(tagName, someHash));
+      tree.createReference(Branch.of(branchName, someHash));
+      tree.createReference(Branch.of(branchName2, someHash));
+
+      List<Reference> references = tree.getAllReferences();
+      Reference tagRef = references.stream().filter(r -> tagName.equals(r.getName())).findFirst().orElse(null);
+      Reference branchRef = references.stream().filter(r -> branchName.equals(r.getName())).findFirst().orElse(null);
+      Reference branchRef2 = references.stream().filter(r -> branchName2.equals(r.getName())).findFirst().orElse(null);
+
+      assertThat(tagRef, instanceOf(Tag.class));
+      assertThat(branchRef, instanceOf(Branch.class));
+      assertThat(branchRef2, instanceOf(Branch.class));
+
+      tagHash = tagRef.getHash();
+      branchHash = branchRef.getHash();
+      branchHash2 = branchRef2.getHash();
+
+      assertThat(tree.getReferenceByName(tagName), equalTo(tagRef));
+      assertThat(tree.getReferenceByName(branchName), equalTo(branchRef));
+
+      EntriesResponse entries = tree.getEntries(tagName);
+      assertThat(entries, notNullValue());
+      entries = tree.getEntries(branchName);
+      assertThat(entries, notNullValue());
+
+      LogResponse log = tree.getCommitLog(tagName);
+      assertThat(log, notNullValue());
+      log = tree.getCommitLog(branchName);
+      assertThat(log, notNullValue());
+
+      // Need to have at least one op, otherwise all following operations (assignTag/Branch, merge, delete) will fail
+      ImmutablePut op = ImmutablePut.builder().key(ContentsKey.of("some-key")).contents(IcebergTable.of("foo")).build();
+      Operations ops = ImmutableOperations.builder().addOperations(op).build();
+      tree.commitMultipleOperations(branchName, branchHash, "One dummy op", ops);
+      log = tree.getCommitLog(branchName);
+      newHash = log.getOperations().get(0).getHash();
+
+      tree.assignTag(tagName, tagHash, Tag.of(tagName, newHash));
+      tree.assignBranch(branchName, newHash, Branch.of(branchName, newHash));
+
+      tree.mergeRefIntoBranch(branchName2, branchHash2, ImmutableMerge.builder().fromHash(newHash).build());
+
+      tree.deleteTag(tagName, newHash);
+      tree.deleteBranch(branchName, newHash);
+    } finally {
+      try {
+        tree.deleteBranch(branchName, null);
+      } catch (Exception ignore) {
+        // ignore cleanup exceptions
+      }
+      try {
+        tree.deleteBranch(branchName2, null);
+      } catch (Exception ignore) {
+        // ignore cleanup exceptions
+      }
+      try {
+        tree.deleteTag(tagName, tagHash);
+      } catch (Exception ignore) {
+        // ignore cleanup exceptions
+      }
+      try {
+        tree.deleteTag(tagName, newHash);
+      } catch (Exception ignore) {
+        // ignore cleanup exceptions
+      }
+    }
   }
 
   @Test
