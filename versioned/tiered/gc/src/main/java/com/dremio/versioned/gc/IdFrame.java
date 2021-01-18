d065e179798c70b5f5ca373e4568f48f3109cee3
Jacques Nadeau <jacques@apache.org>
2021-01-18 14:30:53 -0800

    Implement Garbage Collection Algorithm

---
+++ versioned/tiered/gc/src/main/java/com/dremio/versioned/gc/IdFrame.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright (C) 2020 Dremio
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.dremio.versioned.gc;
+
+import java.util.Arrays;
+
+import com.dremio.nessie.versioned.store.Id;
+
+public class IdFrame {
+
+  private byte[] id;
+
+  public byte[] getId() {
+    return id;
+  }
+
+  public void setId(byte[] id) {
+    this.id = id;
+  }
+
+  public IdFrame() {
+  }
+
+  public IdFrame(byte[] id) {
+    this.id = id;
+  }
+
+  public static IdFrame of(Id id) {
+    return new IdFrame(id.toBytes());
+  }
+
+  @Override
+  public String toString() {
+    return Id.of(id).toString();
+  }
+
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + Arrays.hashCode(id);
+    return result;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj) {
+      return true;
+    }
+    if (!(obj instanceof IdFrame)) {
+      return false;
+    }
+    IdFrame other = (IdFrame) obj;
+    return Arrays.equals(id, other.id);
+  }
+
+
+}
---
+++ versioned/tiered/gc/src/main/java/com/dremio/versioned/gc/ValueFrame.java
@@ -0,0 +1,77 @@
+package com.dremio.versioned.gc;
+
+import java.util.Optional;
+import java.util.function.Function;
+import java.util.function.Supplier;
+
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.SparkSession;
+
+import com.dremio.nessie.tiered.builder.Value;
+import com.dremio.nessie.versioned.store.Id;
+import com.dremio.nessie.versioned.store.Store;
+import com.dremio.nessie.versioned.store.ValueType;
+import com.google.protobuf.ByteString;
+
+public class ValueFrame {
+  private byte[] bytes;
+  private long dt;
+  private IdFrame id;
+
+  public byte[] getBytes() {
+    return bytes;
+  }
+
+  public long getDt() {
+    return dt;
+  }
+
+  public void setDt(long dt) {
+    this.dt = dt;
+  }
+
+  public void setBytes(byte[] bytes) {
+    this.bytes = bytes;
+  }
+
+  public IdFrame getId() {
+    return id;
+  }
+
+  public void setId(IdFrame id) {
+    this.id = id;
+  }
+
+  public ValueFrame() {
+  }
+
+  public static Function<Store.Acceptor<Value>, ValueFrame> BUILDER = (a) -> {
+    ValueFrame f = new ValueFrame();
+    a.applyValue(new Value() {
+
+      @Override
+      public Value value(ByteString value) {
+        f.bytes = value.toByteArray();
+        return this;
+      }
+
+      @Override
+      public Value id(Id id) {
+        f.id = IdFrame.of(id);
+        return this;
+      }
+
+      @Override
+      public Value dt(long dt) {
+        f.dt = dt;
+        return null;
+      }
+    });
+    return f;
+  };
+
+  public static Dataset<ValueFrame> asDataset(Supplier<Store> store, SparkSession spark) {
+    return ValueRetriever.dataset(store, ValueType.VALUE, ValueFrame.class, Optional.empty(), spark, BUILDER);
+  }
+
+}
