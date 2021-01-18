0849dec9a0e7a75a77434cd0dadbde4dfd4eedc1
Jacques Nadeau <jacques@apache.org>
2021-01-18 15:24:16 -0800

    fix checkstyle

--- versioned/tiered/tiered-impl/src/main/java/com/dremio/nessie/versioned/impl/InternalL1.java
+++ versioned/tiered/tiered-impl/src/main/java/com/dremio/nessie/versioned/impl/InternalL1.java
@@ -32,7 +32,8 @@ class InternalL1 extends PersistentBase<L1> {
   private static final long HASH_SEED = 3506039963025592061L;
 
   static final int SIZE = 43;
-  static InternalL1 EMPTY = new InternalL1(Id.EMPTY, new IdMap(SIZE, InternalL2.EMPTY_ID), null, KeyList.EMPTY, ParentList.EMPTY, DT.UNKNOWN);
+  static InternalL1 EMPTY =
+      new InternalL1(Id.EMPTY, new IdMap(SIZE, InternalL2.EMPTY_ID), null, KeyList.EMPTY, ParentList.EMPTY, DT.UNKNOWN);
   static Id EMPTY_ID = EMPTY.getId();
 
   private final IdMap tree;
