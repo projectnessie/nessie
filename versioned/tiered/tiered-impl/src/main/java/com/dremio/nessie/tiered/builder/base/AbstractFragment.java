d848915641d869ca32aa98325b07dbc1817d4b23
Robert Stupp <snazy@snazy.de>
2021-01-22 10:39:30 +0100

    More rebase-refactoring due to the "dt adjusting Java-Proxy" from the GC patch

---
+++ versioned/tiered/tiered-impl/src/main/java/com/dremio/nessie/tiered/builder/base/AbstractFragment.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright (C) 2020 Dremio
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.dremio.nessie.tiered.builder.base;
+
+import java.util.stream.Stream;
+
+import com.dremio.nessie.tiered.builder.Fragment;
+import com.dremio.nessie.versioned.Key;
+
+public abstract class AbstractFragment extends AbstractBaseValue<Fragment> implements Fragment {
+  @Override
+  public Fragment keys(Stream<Key> keys) {
+    return this;
+  }
+}
---
+++ versioned/tiered/tiered-impl/src/main/java/com/dremio/nessie/tiered/builder/base/AbstractRef.java
@@ -0,0 +1,105 @@
+/*
+ * Copyright (C) 2020 Dremio
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.dremio.nessie.tiered.builder.base;
+
+import com.dremio.nessie.versioned.Key.Mutation;
+import java.util.function.Consumer;
+import java.util.stream.Stream;
+
+import com.dremio.nessie.tiered.builder.Ref;
+import com.dremio.nessie.versioned.store.Id;
+
+public abstract class AbstractRef extends AbstractBaseValue<Ref> implements Ref {
+
+  @Override
+  public Ref name(String name) {
+    return this;
+  }
+
+  public abstract static class AbstractTag implements Tag {
+    @Override
+    public Tag commit(Id commit) {
+      return this;
+    }
+  }
+
+  public abstract static class AbstractBranch implements Branch {
+    @Override
+    public Branch metadata(Id metadata) {
+      return this;
+    }
+
+    @Override
+    public Branch children(Stream<Id> children) {
+      return this;
+    }
+
+    @Override
+    public Branch commits(Consumer<BranchCommit> commits) {
+      return this;
+    }
+  }
+
+  public abstract static class AbstractUnsavedCommitMutations implements UnsavedCommitMutations {
+    protected final BranchCommit branchCommit;
+
+    protected AbstractUnsavedCommitMutations(BranchCommit branchCommit) {
+      this.branchCommit = branchCommit;
+    }
+
+    @Override
+    public UnsavedCommitMutations keyMutation(Mutation keyMutation) {
+      return this;
+    }
+
+    @Override
+    public BranchCommit done() {
+      return branchCommit;
+    }
+  }
+
+  public abstract static class AbstractUnsavedCommitDelta implements UnsavedCommitDelta {
+    protected final BranchCommit branchCommit;
+
+    protected AbstractUnsavedCommitDelta(BranchCommit branchCommit) {
+      this.branchCommit = branchCommit;
+    }
+
+    @Override
+    public UnsavedCommitDelta delta(int position, Id oldId, Id newId) {
+      return this;
+    }
+  }
+
+  public abstract static class AbstractSavedCommit implements SavedCommit {
+    protected final BranchCommit branchCommit;
+
+    protected AbstractSavedCommit(BranchCommit branchCommit) {
+      this.branchCommit = branchCommit;
+    }
+
+    @Override
+    public SavedCommit parent(Id parent) {
+      return this;
+    }
+
+    @Override
+    public BranchCommit done() {
+      return branchCommit;
+    }
+  }
+}
