fd5ffae247c48adfcf3d24489dac6a9f32c91209
Bruno Paiva Lima da Silva <bruno.paivalimadasilva@dremio.com>
2021-02-02 12:42:28 -0800

    Moved TableCommitMetaStoreWorker to nessie-services

--- servers/services/src/main/java/com/dremio/nessie/services/providers/TableCommitMetaStoreWorker.java
+++ servers/services/src/main/java/com/dremio/nessie/services/providers/TableCommitMetaStoreWorker.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.dremio.nessie.server.providers;
+package com.dremio.nessie.services.providers;
 
 import java.io.IOException;
 import java.util.stream.Collectors;
