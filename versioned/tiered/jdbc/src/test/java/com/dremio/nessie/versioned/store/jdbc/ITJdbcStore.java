1012b310c97099dda9ca6875449ba19a4eb89162
Robert Stupp <snazy@snazy.de>
2021-02-02 19:09:10 +0100

    Add test to illustrate the behavior when more than 15 branch-commits are present in a `Ref.branch()`

--- versioned/tiered/jdbc/src/test/java/com/dremio/nessie/versioned/store/jdbc/ITJdbcStore.java
+++ versioned/tiered/jdbc/src/test/java/com/dremio/nessie/versioned/store/jdbc/ITJdbcStore.java
@@ -15,10 +15,30 @@
  */
 package com.dremio.nessie.versioned.store.jdbc;
 
+import static org.hamcrest.CoreMatchers.anyOf;
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.sql.SQLException;
+import java.util.Arrays;
+import java.util.Locale;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+import org.hamcrest.Matcher;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
 
+import com.dremio.nessie.tiered.builder.Ref;
 import com.dremio.nessie.versioned.impl.AbstractTestStore;
+import com.dremio.nessie.versioned.store.Id;
+import com.dremio.nessie.versioned.store.SaveOp;
+import com.dremio.nessie.versioned.store.StoreOperationException;
+import com.dremio.nessie.versioned.store.ValueType;
 
 /**
  * Runs basic store-tests against a database in a test-container.
@@ -70,5 +90,38 @@ class ITJdbcStore extends AbstractTestStore<JdbcStore> {
   @Override
   protected void resetStoreState() {
     super.store = null;
+  }
+
+  @Test
+  void branchCommitOverflow() {
+    Throwable t = assertThrows(StoreOperationException.class, () -> {
+      Id id = Id.generateRandom();
+      store.putIfAbsent(new SaveOp<Ref>(ValueType.REF, id) {
+        @Override
+        public void serialize(Ref consumer) {
+          consumer.id(id)
+              .dt(System.currentTimeMillis() * 1000L)
+              .name("branch-commit-overflow")
+              .branch()
+              .metadata(Id.generateRandom())
+              .children(IntStream.range(0, 43).mapToObj(ignore -> Id.generateRandom()))
+              .commits(bc -> {
+                for (int i = 0; i < JdbcRef.MAX_COMMITS + 1; i++) {
+                  bc.saved().parent(Id.generateRandom()).done();
+                }
+              });
+        }
+      });
+    });
+    t = t.getCause();
+    assertThat(t, instanceOf(SQLError.class));
+    t = t.getCause();
+    assertThat(t, instanceOf(SQLException.class));
+
+    // the database/driver should complain that one of the C_* columns does not exist
+    Iterable<Matcher<? super String>> matchers = Arrays.stream(JdbcRef.C_COLUMNS)
+        .flatMap(c -> Stream.of(containsString(c + "_15"), containsString(c.toUpperCase(Locale.ROOT) + "_15")))
+        .collect(Collectors.toList());
+    assertThat(t.getMessage(), anyOf(matchers));
   }
 }
