4568bca077b08e8d02683c8d3ff31d3801866d3e
Laurent Goujon <laurent@dremio.com>
2021-03-04 20:56:35 -0800

    Change TreeResource constructor visibility

--- servers/services/src/main/java/org/projectnessie/services/rest/TreeResource.java
+++ servers/services/src/main/java/org/projectnessie/services/rest/TreeResource.java
@@ -70,7 +70,7 @@ import com.google.common.collect.ImmutableList;
 public class TreeResource extends BaseResource implements TreeApi {
 
   @Inject
-  protected TreeResource(ServerConfig config, Principal principal,
+  public TreeResource(ServerConfig config, Principal principal,
       VersionStore<Contents, CommitMeta> store) {
     super(config, principal, store);
   }
