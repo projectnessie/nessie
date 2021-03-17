f444399262c5acb910ebc675dd5b396c98f700ed
Robert Stupp <snazy@snazy.de>
2021-03-17 17:56:25 +0100

    Add explicit test
    Fails with the following exception, if the line
    ```
    unsavedL1s.put(lastL1.getId(), lastL1);
    ```
    is commented out in `org.projectnessie.versioned.impl.InternalBranch#getUpdateState`. That change would effectively make the prod-code change a no-op.
    
    ```
    org.projectnessie.versioned.store.NotFoundException: Unable to find requested ref.
    	at org.projectnessie.versioned.dynamodb.DynamoStore.load(DynamoStore.java:228)
    	at org.projectnessie.versioned.impl.HistoryRetriever$HistoryIterator.calculateNextList(HistoryRetriever.java:200)
    	at org.projectnessie.versioned.impl.HistoryRetriever$HistoryIterator.computeNext(HistoryRetriever.java:136)
    	at org.projectnessie.versioned.impl.HistoryRetriever$HistoryIterator.computeNext(HistoryRetriever.java:110)
    	at com.google.common.collect.AbstractIterator.tryToComputeNext(AbstractIterator.java:141)
    	at com.google.common.collect.AbstractIterator.hasNext(AbstractIterator.java:136)
    	at java.base/java.util.Iterator.forEachRemaining(Iterator.java:132)
    	at java.base/java.util.Spliterators$IteratorSpliterator.forEachRemaining(Spliterators.java:1801)
    	at java.base/java.util.stream.AbstractPipeline.copyInto(AbstractPipeline.java:484)
    	at java.base/java.util.stream.AbstractPipeline.wrapAndCopyInto(AbstractPipeline.java:474)
    	at java.base/java.util.stream.ReduceOps$ReduceOp.evaluateSequential(ReduceOps.java:913)
    	at java.base/java.util.stream.AbstractPipeline.evaluate(AbstractPipeline.java:234)
    	at java.base/java.util.stream.ReferencePipeline.collect(ReferencePipeline.java:578)
    	at org.projectnessie.versioned.impl.KeyList$IncrementalList.getKeysIter(KeyList.java:145)
    	at org.projectnessie.versioned.impl.KeyList$IncrementalList.generateNewCheckpoint(KeyList.java:123)
    	at org.projectnessie.versioned.impl.KeyList$IncrementalList.createCheckpointIfNeeded(KeyList.java:107)
    	at org.projectnessie.versioned.impl.InternalL1.withCheckpointAsNecessary(InternalL1.java:68)
    	at org.projectnessie.versioned.impl.InternalBranch.getUpdateState(InternalBranch.java:277)
    	at org.projectnessie.versioned.impl.AbstractITTieredVersionStore.checkpointWithUnsavedL1(AbstractITTieredVersionStore.java:314)
    ```

--- versioned/tiered/tiered-tests/src/main/java/org/projectnessie/versioned/impl/AbstractITTieredVersionStore.java
+++ versioned/tiered/tiered-tests/src/main/java/org/projectnessie/versioned/impl/AbstractITTieredVersionStore.java
@@ -19,6 +19,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.Arrays;
 import java.util.Collections;
@@ -26,6 +27,9 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Optional;
 import java.util.Random;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -43,11 +47,13 @@ import org.projectnessie.versioned.ImmutablePut;
 import org.projectnessie.versioned.ImmutableTagName;
 import org.projectnessie.versioned.Key;
 import org.projectnessie.versioned.NamedRef;
+import org.projectnessie.versioned.Operation;
 import org.projectnessie.versioned.Put;
 import org.projectnessie.versioned.Ref;
 import org.projectnessie.versioned.ReferenceAlreadyExistsException;
 import org.projectnessie.versioned.ReferenceConflictException;
 import org.projectnessie.versioned.ReferenceNotFoundException;
+import org.projectnessie.versioned.Serializer;
 import org.projectnessie.versioned.TagName;
 import org.projectnessie.versioned.Unchanged;
 import org.projectnessie.versioned.VersionStore;
@@ -55,6 +61,7 @@ import org.projectnessie.versioned.WithHash;
 import org.projectnessie.versioned.impl.InconsistentValue.InconsistentValueException;
 import org.projectnessie.versioned.store.Id;
 import org.projectnessie.versioned.store.Store;
+import org.projectnessie.versioned.store.ValueType;
 
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
@@ -287,6 +294,88 @@ public abstract class AbstractITTieredVersionStore {
 
     // can't use tag delete on branch.
     assertThrows(ReferenceConflictException.class, () -> versionStore().delete(TagName.of("foo"), Optional.empty()));
+  }
+
+  @Test
+  void checkpointWithUnsavedL1() throws Exception {
+    // KeyList.IncrementalList.generateNewCheckpoint collects parent L1s for a branch
+    // via HistoryRetriever to "checkpoint" the keylist. However, this only works if
+    // HistoryRetriever has access to both saved AND unsaved L1s, so L1s that are persisted
+    // and those that are still in the branch's REF. This test verifies that generateNewCheckpoint
+    // does not fail in that case.
+
+    ExecutorService executor = Executors.newCachedThreadPool();
+    try {
+
+      BranchName branch = BranchName.of("checkpointWithUnsavedL1");
+
+      versionStore().create(branch, Optional.empty());
+
+      InternalRefId ref = InternalRefId.of(branch);
+
+      // generate MAX_DELTAS-1 keys in the key-list - just enough to *NOT*
+      // trigger KeyList.IncrementalList.generateNewCheckpoint
+      for (int i = 1; i < KeyList.IncrementalList.MAX_DELTAS; i++) {
+        InternalBranch internalBranch = simulateCommit(ref, executor, i);
+
+        // verify that the branch has an unsaved L1
+        assertEquals(i, internalBranch.getCommits().stream().filter(c -> !c.isSaved()).count());
+        KeyList keyList = internalBranch.getUpdateState(store()).unsafeGetL1().getKeyList();
+        assertFalse(keyList.isFull());
+        assertFalse(keyList.isEmptyIncremental());
+        KeyList.IncrementalList incrementalList = (KeyList.IncrementalList) keyList;
+        assertEquals(i, incrementalList.getDistanceFromCheckpointCommits());
+      }
+
+      InternalBranch internalBranch = simulateCommit(ref, executor, KeyList.IncrementalList.MAX_DELTAS);
+      KeyList keyList = internalBranch.getUpdateState(store()).unsafeGetL1().getKeyList();
+      assertTrue(keyList.isFull());
+      assertFalse(keyList.isEmptyIncremental());
+    } finally {
+      executor.shutdown();
+    }
+
+  }
+
+  private InternalBranch simulateCommit(InternalRefId ref, Executor executor, int num) {
+    List<Operation<String>> ops = Collections.singletonList(Put.of(Key.of("key" + num), "foo" + num));
+    List<InternalKey> keys = ops.stream().map(op -> new InternalKey(op.getKey())).collect(Collectors.toList());
+
+    Serializer<String> serializer = AbstractTieredStoreFixture.WORKER.getValueSerializer();
+    Serializer<String> metadataSerializer = AbstractTieredStoreFixture.WORKER.getMetadataSerializer();
+
+    PartialTree<String> current = PartialTree.of(serializer, ref, keys);
+
+    String incomingCommit = "metadata";
+    InternalCommitMetadata metadata = InternalCommitMetadata.of(metadataSerializer.toBytes(incomingCommit));
+
+    store().load(current.getLoadChain(b -> {
+      InternalBranch.UpdateState updateState = b.getUpdateState(store());
+      return updateState.unsafeGetL1();
+    }, PartialTree.LoadType.NO_VALUES));
+
+    // do updates.
+    ops.forEach(op ->
+        current.setValueForKey(new InternalKey(op.getKey()), Optional.of(((Put<String>) op).getValue())));
+
+    // save all but l1 and branch.
+    store().save(
+        Stream.concat(
+            current.getMostSaveOps(),
+            Stream.of(EntityType.COMMIT_METADATA.createSaveOpForEntity(metadata))
+        ).collect(Collectors.toList()));
+
+    PartialTree.CommitOp commitOp = current.getCommitOp(
+        metadata.getId(),
+        Collections.emptyList(),
+        true,
+        true);
+
+    InternalRef.Builder<?> builder = EntityType.REF.newEntityProducer();
+    boolean updated = store().update(ValueType.REF, ref.getId(),
+        commitOp.getUpdateWithCommit(), Optional.of(commitOp.getTreeCondition()), Optional.of(builder));
+    assertTrue(updated);
+    return builder.build().getBranch();
   }
 
   @Test
