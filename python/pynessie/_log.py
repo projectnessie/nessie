56b181265f6b9a8e4552a413d84489c0f3c9c124
Eduard Tudenhoefner <etudenhoefner@gmail.com>
2021-05-27 15:54:51 +0200

    flake8

--- python/pynessie/_log.py
+++ python/pynessie/_log.py
@@ -1,12 +1,10 @@
 # -*- coding: utf-8 -*-
-from datetime import datetime
 from typing import Any
 from typing import Generator
 from typing import Optional
 from typing import Tuple
 
 import click
-from dateutil.parser import parse
 
 from pynessie import NessieClient
 from pynessie.model import CommitMeta
@@ -18,6 +16,7 @@ def show_log(
     number: Optional[int] = -1,
     after: Optional[str] = None,
     before: Optional[str] = None,
+    author: Optional[str] = None,
     committer: Optional[str] = None,
     end: Optional[str] = None,
     limits: Tuple[click.Path] = None,
@@ -27,7 +26,7 @@ def show_log(
     Note:
         limiting by path is not yet supported.
     """
-    raw_log = nessie.get_log(start)
+    raw_log = nessie.get_log(start_ref=start, author=author, committer=committer, before=before, after=after)
 
     def generator() -> Generator[CommitMeta, Any, None]:
         for i in raw_log:
