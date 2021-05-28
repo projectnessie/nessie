0a929b78ec641e943426c965ce789a64d6768591
Robert Stupp <snazy@snazy.de>
2021-05-28 15:12:33 +0200

    Update Maven+Gradle plugins to launch the Nessie-Server via `java -jar`
    Before this PR, the apprunner-gradle-plugin uses Quarkus mechanisms to resolve artifacts, setup the configuration, etc. - this
    reuses functionality from the apprunner-maven-plugin. Since #1245 (new in Nessie 0.6.1), the Nessie-Server is built with Java 11.
    The Iceberg-Java-8 build (naturally) fails with Nessie 0.6.1 (comment). It's not a solution to revert #1245, because Quarkus 2.0
    will drop Java 8 support.
    
    The idea is to launch the Nessie server as a separate process using the uber-jar, which is published to Maven Central since Nessie 0.6.1.
    
    Removing all the Quarkus-bootstrap code from the plugin simplifies the plugin code a lot and removes the amount of required dependencies.
    
    There are 2 "official" and 1 "private" way to specify the Nessie-Quarkus jar to run:
    1. In Maven by specifying the coordinates via the `appArtifactId` config options, in Gradle by declaring the
      dependency via the `nessieQuarkusServer` configuration).
    2. In Maven or Gradle by providing the path to the jar via the `executableJar` config option.
    3. Only in Maven and only for Nessie-source-tree builds: neither 1 nor 2, the Maven plugin figures out the path on its own.
    
    A new module `nessie-apprunner-common` contains the underlying code to find the JVM via environment variables or system properties.
    The Maven tries to find a matching JDK via the Maven toolkits API and falls back to the `JavaVM` implementation. The Gradle plugin
    always uses the `JavaVM` implementation (Gradle 5.4 doesn't have that functionality, but newer Gradle version do IIRC).
    That module also contains code to start the Java process running the Nessie-Server-Quarkus-jar, capture the http listen URL and provide
    both the full URL and port via project properties to associated test tasks.
    
    The overall implementation makes it much easier to reason about system properties and environment variables passed to the executable
    Nessie-Quarkus-jar. With the Quarkus-bootstrap code, it was hard to reason about and debug issues with properties passed to Quarkus and/or
    Nessie.
    
    The Maven plugin also got some integration tests.
    
    Fixes #1333

--- tools/apprunner-maven-plugin/src/main/java/org/projectnessie/quarkus/maven/AbstractQuarkusAppMojo.java
+++ tools/apprunner-maven-plugin/src/main/java/org/projectnessie/quarkus/maven/AbstractQuarkusAppMojo.java
@@ -15,11 +15,11 @@
  */
 package org.projectnessie.quarkus.maven;
 
-import io.quarkus.bootstrap.model.AppArtifact;
-import io.quarkus.bootstrap.model.AppArtifactCoords;
+import org.apache.maven.execution.MavenSession;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.project.MavenProject;
+import org.projectnessie.quarkus.runner.ProcessHandler;
 
 /*
  * Base class to share configuration between mojo.
@@ -30,6 +30,10 @@ abstract class AbstractQuarkusAppMojo extends AbstractMojo {
   /** Maven project. */
   @Parameter(defaultValue = "${project}", readonly = true, required = true)
   private MavenProject project;
+
+  /** Maven session. */
+  @Parameter(defaultValue = "${session}", readonly = true, required = true)
+  private MavenSession session;
 
   /** Whether execution should be skipped. */
   @Parameter(property = "nessie.apprunner.skip", required = false, defaultValue = "false")
@@ -51,14 +55,18 @@ abstract class AbstractQuarkusAppMojo extends AbstractMojo {
     return project;
   }
 
+  public MavenSession getSession() {
+    return session;
+  }
+
   private String getContextKey() {
     final String key = CONTEXT_KEY + '.' + getExecutionId();
     return key;
   }
 
-  protected AutoCloseable getApplication() {
+  protected ProcessHandler getApplication() {
     final String key = getContextKey();
-    return (AutoCloseable) project.getContextValue(key);
+    return (ProcessHandler) project.getContextValue(key);
   }
 
   protected void resetApplication() {
@@ -66,24 +74,15 @@ abstract class AbstractQuarkusAppMojo extends AbstractMojo {
     project.setContextValue(key, null);
   }
 
-  protected void setApplicationHandle(AutoCloseable application) {
+  protected void setApplicationHandle(ProcessHandler application) {
     final String key = getContextKey();
     final Object previous = project.getContextValue(key);
     if (previous != null) {
       getLog()
           .warn(
-              String.format("Found a previous application for execution id %s.", getExecutionId()));
+              String.format(
+                  "Found a previous ProcessHandler for execution id %s.", getExecutionId()));
     }
     project.setContextValue(key, application);
-  }
-
-  static AppArtifact fromString(String artifactId) {
-    AppArtifactCoords coords = AppArtifactCoords.fromString(artifactId);
-    return new AppArtifact(
-        coords.getGroupId(),
-        coords.getArtifactId(),
-        coords.getClassifier(),
-        coords.getType(),
-        coords.getVersion());
   }
 }
