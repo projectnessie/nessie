9b542ddc9a960282d97a1c96e471c9b224abf549
Robert Stupp <snazy@snazy.de>
2021-07-22 18:45:09 +0200

    TVS2: Services (REST API server side) changes

--- servers/services/src/main/java/org/projectnessie/services/rest/BaseResource.java
+++ servers/services/src/main/java/org/projectnessie/services/rest/BaseResource.java
@@ -16,14 +16,14 @@
 package org.projectnessie.services.rest;
 
 import java.security.Principal;
-import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
-import javax.annotation.Nullable;
 import javax.ws.rs.core.SecurityContext;
+import org.projectnessie.error.NessieConflictException;
 import org.projectnessie.error.NessieNotFoundException;
 import org.projectnessie.model.CommitMeta;
 import org.projectnessie.model.Contents;
+import org.projectnessie.model.GlobalContents;
+import org.projectnessie.model.Validation;
 import org.projectnessie.services.config.ServerConfig;
 import org.projectnessie.versioned.Hash;
 import org.projectnessie.versioned.NamedRef;
@@ -37,7 +37,7 @@ abstract class BaseResource {
 
   private final MultiTenant multiTenant;
 
-  private final VersionStore<Contents, CommitMeta, Contents.Type> store;
+  private final VersionStore<Contents, GlobalContents, CommitMeta, Contents.Type> store;
 
   // Mandated by CDI 2.0
   protected BaseResource() {
@@ -47,72 +47,57 @@ abstract class BaseResource {
   protected BaseResource(
       ServerConfig config,
       MultiTenant multiTenant,
-      VersionStore<Contents, CommitMeta, Contents.Type> store) {
+      VersionStore<Contents, GlobalContents, CommitMeta, Contents.Type> store) {
     this.config = config;
     this.multiTenant = multiTenant;
     this.store = store;
   }
 
-  Optional<Hash> getHash(String ref) {
-    try {
-      WithHash<Ref> whr = store.toRef(Optional.ofNullable(ref).orElse(config.getDefaultBranch()));
-      return Optional.of(whr.getHash());
-    } catch (ReferenceNotFoundException e) {
+  NamedRef namedRefOrThrow(String ref) throws NessieNotFoundException {
+    return (NamedRef) namedRefWithHashOrThrow(ref).getValue();
+  }
+
+  Optional<NamedRef> optionalNamedRefOrThrow(String ref) throws NessieNotFoundException {
+    if (ref == null || ref.isEmpty()) {
       return Optional.empty();
     }
+    return Optional.of((NamedRef) namedRefWithHashOrThrow(ref).getValue());
   }
 
-  WithHash<NamedRef> namedRefWithHashOrThrow(String namedRef, @Nullable String hashOnRef)
-      throws NessieNotFoundException {
-    List<WithHash<NamedRef>> collect =
-        store
-            .getNamedRefs()
-            .filter(
-                r ->
-                    r.getValue().getName().equals(namedRef)
-                        || r.getValue().getName().equals(config.getDefaultBranch()))
-            .collect(Collectors.toList());
-    WithHash<NamedRef> namedRefWithHash;
-    if (collect.size() == 1) {
-      namedRefWithHash = collect.get(0);
-    } else {
-      namedRefWithHash =
-          collect.stream()
-              .filter(r -> r.getValue().getName().equals(namedRef))
-              .findFirst()
-              .orElseThrow(
-                  () ->
-                      new NessieNotFoundException(String.format("Ref for %s not found", namedRef)));
-    }
-
+  WithHash<Ref> namedRefWithHashOrThrow(String ref) throws NessieNotFoundException {
     try {
-      if (null == hashOnRef) {
-        return namedRefWithHash;
+      if (null != ref) {
+        ref = Validation.validateReferenceName(ref);
       }
-
-      // we need to make sure that the hash in fact exists on the named ref
-      Hash hash = Hash.of(hashOnRef);
-      if (store.getCommits(namedRefWithHash.getValue()).noneMatch(c -> c.getHash().equals(hash))) {
-        throw new NessieNotFoundException(
-            String.format("Hash %s on Ref %s could not be found", hashOnRef, namedRef));
-      }
-      return WithHash.of(hash, namedRefWithHash.getValue());
+      return store.toRef(Optional.ofNullable(ref).orElse(config.getDefaultBranch()));
     } catch (ReferenceNotFoundException e) {
-      throw new NessieNotFoundException(
-          String.format("Hash %s on Ref %s could not be found", hashOnRef, namedRef));
+      throw new NessieNotFoundException(String.format("Ref for %s not found", ref));
     }
   }
 
-  Hash getHashOrThrow(String ref) throws NessieNotFoundException {
-    return getHash(ref)
-        .orElseThrow(() -> new NessieNotFoundException(String.format("Ref for %s not found", ref)));
+  static Optional<Hash> toHash(String hash, boolean required) throws NessieConflictException {
+    try {
+      return toHashIA(hash, required);
+    } catch (IllegalArgumentException e) {
+      throw new NessieConflictException("Must provide expected hash value for operation.");
+    }
+  }
+
+  static Optional<Hash> toHashIA(String hash, boolean required) {
+    if (hash == null || hash.isEmpty()) {
+      if (required) {
+        throw new IllegalArgumentException("Must provide expected hash value for operation.");
+      }
+      return Optional.empty();
+    }
+    return Optional.of(Hash.of(Validation.validateHash(hash)));
   }
 
   protected ServerConfig getConfig() {
     return config;
   }
 
-  protected VersionStore<Contents, CommitMeta, Contents.Type> getStore() {
+  protected VersionStore<Contents, GlobalContents, CommitMeta, Contents.Type> getStore() {
     return store;
   }
 
