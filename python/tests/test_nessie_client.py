61b336d697cc4ab52996b871660ae56102e8c4fd
Robert Stupp <snazy@snazy.de>
2021-08-03 20:16:43 +0200

    pynessie changes

--- python/tests/test_nessie_client.py
+++ python/tests/test_nessie_client.py
@@ -18,17 +18,18 @@ def test_client_interface_e2e() -> None:
     references = client.list_references()
     assert len(references) == 1
     assert references[0] == Branch("main", references[0].hash_)
+    main_name = references[0].name
     main_commit = references[0].hash_
     with pytest.raises(NessieConflictException):
         client.create_branch("main")
-    created_reference = client.create_branch("test", main_commit)
+    created_reference = client.create_branch("test", main_name, main_commit)
     references = client.list_references()
     assert len(references) == 2
     assert next(i for i in references if i.name == "main") == Branch("main", main_commit)
     assert next(i for i in references if i.name == "test") == Branch("test", main_commit)
     reference = client.get_reference("test")
     assert created_reference == reference
-    tables = client.list_keys(reference.name)
+    tables = client.list_keys(reference.name, reference.hash_)
     assert isinstance(tables, Entries)
     assert len(tables.entries) == 0
     client.delete_branch("test", main_commit)
