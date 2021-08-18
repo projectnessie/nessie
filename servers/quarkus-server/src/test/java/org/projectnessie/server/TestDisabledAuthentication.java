23c79100663b0382079eb940255676912f425e5c
Dmitri Bourlatchkov <dmitri.bourlatchkov@dremio.com>
2021-08-18 15:48:44 -0400

    Add NessieAuthenticationMechanism

---
+++ servers/quarkus-server/src/test/java/org/projectnessie/server/TestDisabledAuthentication.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright (C) 2020 Dremio
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.projectnessie.server;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.projectnessie.client.NessieClient.AuthType.BASIC;
+import static org.projectnessie.client.NessieClient.AuthType.BEARER;
+import static org.projectnessie.client.NessieClient.AuthType.NONE;
+
+import com.google.common.collect.ImmutableMap;
+import io.quarkus.test.junit.QuarkusTest;
+import io.quarkus.test.junit.QuarkusTestProfile;
+import io.quarkus.test.junit.TestProfile;
+import java.util.Map;
+import org.junit.jupiter.api.Test;
+
+/**
+ * This test validates that setting `nessie.auth.enabled=false` allows all requests regardless of
+ * their authentication type.
+ */
+@QuarkusTest
+@TestProfile(value = TestDisabledAuthentication.Profile.class)
+public class TestDisabledAuthentication extends BaseClientAuthTest {
+
+  @Test
+  void testBasic() {
+    withClientCustomizer(
+        c -> c.withAuthType(BASIC).withUsername("any_user").withPassword("any_password"));
+    assertThat(client().getTreeApi().getAllReferences()).isNotEmpty();
+  }
+
+  @Test
+  void testBearer() {
+    withClientCustomizer(c -> c.withAuthType(BEARER).withBearerToken("any_token"));
+    assertThat(client().getTreeApi().getAllReferences()).isNotEmpty();
+  }
+
+  @Test
+  void testNone() {
+    withClientCustomizer(c -> c.withAuthType(NONE));
+    assertThat(client().getTreeApi().getAllReferences()).isNotEmpty();
+  }
+
+  public static class Profile implements QuarkusTestProfile {
+    @Override
+    public Map<String, String> getConfigOverrides() {
+      return ImmutableMap.of("nessie.auth.enabled", "false");
+    }
+  }
+}
