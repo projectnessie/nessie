525ddef598a207c02f48f5ad8a89c6f1386687bc
Eduard Tudenhoefner <etudenhoefner@gmail.com>
2021-08-23 10:42:01 +0200

    Replace usage of deprecated ConfigProperties with ConfigMapping

--- servers/quarkus-server/src/main/java/org/projectnessie/server/config/DynamoVersionStoreConfig.java
+++ servers/quarkus-server/src/main/java/org/projectnessie/server/config/DynamoVersionStoreConfig.java
@@ -15,20 +15,24 @@
  */
 package org.projectnessie.server.config;
 
-import io.quarkus.arc.config.ConfigProperties;
-import org.eclipse.microprofile.config.inject.ConfigProperty;
+import io.smallrye.config.ConfigMapping;
+import io.smallrye.config.WithDefault;
+import io.smallrye.config.WithName;
 import org.projectnessie.versioned.dynamodb.DynamoStoreConfig;
 
 /** DynamoDB version store configuration. */
-@ConfigProperties(prefix = "nessie.version.store.dynamo")
+@ConfigMapping(prefix = "nessie.version.store.dynamo")
 public interface DynamoVersionStoreConfig {
 
-  @ConfigProperty(name = "initialize", defaultValue = "false")
+  @WithName("initialize")
+  @WithDefault("false")
   boolean isDynamoInitialize();
 
-  @ConfigProperty(defaultValue = DynamoStoreConfig.TABLE_PREFIX)
+  @WithName("table-prefix")
+  @WithDefault(DynamoStoreConfig.TABLE_PREFIX)
   String getTablePrefix();
 
-  @ConfigProperty(name = "tracing", defaultValue = "true")
+  @WithName("tracing")
+  @WithDefault("true")
   boolean enableTracing();
 }
