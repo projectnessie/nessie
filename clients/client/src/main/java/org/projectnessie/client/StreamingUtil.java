cfc551280b79ad880e075e732fe2c3955f8359c6
Robert Stupp <snazy@snazy.de>
2021-09-07 08:50:23 +0200

    Nessie-API: add supporting functions for `NessieApiV1` (#1894)

--- clients/client/src/main/java/org/projectnessie/client/StreamingUtil.java
+++ clients/client/src/main/java/org/projectnessie/client/StreamingUtil.java
@@ -17,17 +17,20 @@ package org.projectnessie.client;
 
 import java.util.OptionalInt;
 import java.util.stream.Stream;
+import javax.annotation.Nullable;
 import javax.validation.constraints.NotNull;
 import org.projectnessie.api.TreeApi;
 import org.projectnessie.api.params.CommitLogParams;
 import org.projectnessie.api.params.EntriesParams;
+import org.projectnessie.client.api.NessieApiV1;
+import org.projectnessie.client.api.PagingBuilder;
 import org.projectnessie.error.NessieNotFoundException;
 import org.projectnessie.model.CommitMeta;
 import org.projectnessie.model.EntriesResponse;
 import org.projectnessie.model.EntriesResponse.Entry;
 import org.projectnessie.model.LogResponse;
 
-/** Helper and utility methods around streaming of {@link TreeApi} et al. */
+/** Helper and utility methods around streaming of {@link NessieApiV1} et al. */
 public final class StreamingUtil {
 
   private StreamingUtil() {
@@ -45,6 +48,7 @@ public final class StreamingUtil {
    * @param entriesParams A wrapper object holding all filtering parameters
    * @return stream of {@link Entry} objects
    */
+  @Deprecated
   public static Stream<Entry> getEntriesStream(
       @NotNull TreeApi treeApi, @NotNull String ref, @NotNull EntriesParams entriesParams)
       throws NessieNotFoundException {
@@ -78,6 +82,7 @@ public final class StreamingUtil {
    * @param commitLogParams A wrapper object holding all filtering parameters for the commit log
    * @return stream of {@link CommitMeta} objects
    */
+  @Deprecated
   public static Stream<CommitMeta> getCommitLogStream(
       @NotNull TreeApi treeApi, @NotNull String ref, @NotNull CommitLogParams commitLogParams)
       throws NessieNotFoundException {
@@ -96,5 +101,78 @@ public final class StreamingUtil {
             commitLogParams.maxRecords() == null
                 ? OptionalInt.empty()
                 : OptionalInt.of(commitLogParams.maxRecords()));
+  }
+
+  /**
+   * Default implementation to return a stream of objects for a ref, functionally equivalent to
+   * calling {@link TreeApi#getEntries(String, EntriesParams)} with manual paging.
+   *
+   * <p>The {@link Stream} returned by {@code getEntriesStream(ref, OptionalInt.empty())}, if not
+   * limited, returns all commit-log entries.
+   *
+   * @param ref a named reference (branch or tag name) or a commit-hash
+   * @return stream of {@link Entry} objects
+   */
+  public static Stream<Entry> getEntriesStream(
+      @NotNull NessieApiV1 api,
+      @NotNull String ref,
+      @Nullable String hashOnRef,
+      @Nullable String queryExpression,
+      OptionalInt maxRecords)
+      throws NessieNotFoundException {
+
+    return new ResultStreamPaginator<>(
+            EntriesResponse::getEntries,
+            (ref1, pageSize, token) ->
+                builderWithPagedQuery(
+                        api.getEntries(), ref1, hashOnRef, pageSize, token, queryExpression)
+                    .submit())
+        .generateStream(ref, maxRecords);
+  }
+
+  /**
+   * Default implementation to return a stream of commit-log entries, functionally equivalent to
+   * calling {@link NessieApiV1#getCommitLog()} with manual paging.
+   *
+   * <p>The {@link Stream} returned by {@code getCommitLogStream(ref, OptionalInt.empty())}, if not
+   * limited, returns all commit-log entries.
+   *
+   * @param api The {@link NessieApiV1} to use
+   * @param ref a named reference (branch or tag name) or a commit-hash
+   * @return stream of {@link CommitMeta} objects
+   */
+  public static Stream<CommitMeta> getCommitLogStream(
+      @NotNull NessieApiV1 api,
+      @NotNull String ref,
+      @Nullable String hashOnRef,
+      @Nullable String untilHash,
+      @Nullable String queryExpression,
+      OptionalInt maxRecords)
+      throws NessieNotFoundException {
+    return new ResultStreamPaginator<>(
+            LogResponse::getOperations,
+            (reference, pageSize, token) ->
+                builderWithPagedQuery(
+                        api.getCommitLog(), reference, hashOnRef, pageSize, token, queryExpression)
+                    .untilHash(untilHash)
+                    .submit())
+        .generateStream(ref, maxRecords);
+  }
+
+  private static <B extends PagingBuilder<B>> B builderWithPagedQuery(
+      B builder,
+      String reference,
+      String hashOnRef,
+      Integer pageSize,
+      String token,
+      String queryExpression) {
+    builder.refName(reference).hashOnRef(hashOnRef).queryExpression(queryExpression);
+    if (pageSize != null) {
+      builder.maxRecords(pageSize);
+    }
+    if (token != null) {
+      builder.pageToken(token);
+    }
+    return builder;
   }
 }
