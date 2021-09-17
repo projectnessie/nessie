6033a56244c16e1b344bedb185ba0a5c5b60e638
Dmitri Bourlatchkov <dmitri.bourlatchkov@dremio.com>
2021-09-17 10:27:24 -0400

    Make error handling more robust in python client (#2009)
    * Use a generic user-facing error message when a 500
      response body cannot be parsed as JSON
    
    Related to #1838

---
+++ python/tests/test_nessie_cli_error.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+"""Authentication tests for Nessi CLI."""
+#  Copyright (C) 2020 Dremio
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#  http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+import pytest
+from click.testing import CliRunner
+
+from .test_nessie_cli import _run
+
+
+# Note: tests in this file use custom VCR files for error server responses.
+# Running these tests in recording mode will likely NOT produce the expected
+# server responses. The related VCR files need to be reviewed and corrected
+# manually.
+
+
+@pytest.mark.vcr
+def test_server_error_html() -> None:
+    """Test the handling of 500 responses with HTML payload (unexpected, but possible)."""
+    runner = CliRunner()
+    result = _run(runner, ["--json", "remote", "show"], ret_val=1)
+    assert "Internal Server Error" in result.output
+    assert "500" in result.output
+
+
+@pytest.mark.vcr
+def test_server_error_json() -> None:
+    """Test the handling of 500 responses with JSON payload."""
+    runner = CliRunner()
+    result = _run(runner, ["--json", "remote", "show"], ret_val=1)
+    assert "Default branch isn't a branch" in result.output
+    assert "500" in result.output
