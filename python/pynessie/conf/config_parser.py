5a8835f1e2a5010b9cc5cf65a1fb410e6ba8e6f6
Dmitri Bourlatchkov <dmitri.bourlatchkov@dremio.com>
2021-09-28 09:15:48 -0400

    Prefer explict args over env. vars in python CLI (#2147)
    Values given as explicit command line parameters
    should take precedence over env. variables for
    the same configuration property.

--- python/pynessie/conf/config_parser.py
+++ python/pynessie/conf/config_parser.py
@@ -19,9 +19,9 @@ def _get_env_args() -> dict:
 def build_config(args: dict = None) -> confuse.Configuration:
     """Build configuration object from input params, env variables and yaml file."""
     config = confuse.Configuration("nessie", __name__)
-    if args:
-        config.set_args(args, dots=True)
     env_args = _get_env_args()
     config.set_args(env_args, dots=True)
+    if args:
+        config.set_args(args, dots=True)
     config["auth"]["password"].redact = True
     return config
