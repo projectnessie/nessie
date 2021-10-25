99c09086ffa3e6afbb743cfe0a268790ba3a7d31
Dmitri Bourlatchkov <dmitri.bourlatchkov@dremio.com>
2021-10-25 11:58:44 -0400

    Handle Nessie error codes in python client (#2476)
    * Add new Nessie Model exceptions to python code
    
    * Remove unnecessary `details` parameters from _endpoints.py
    
    * Rework exception messages and STDOUT representation
    
    * General clean-up of exception construction logic
    
    Closes #477

--- python/pynessie/_endpoints.py
+++ python/pynessie/_endpoints.py
@@ -3,21 +3,13 @@
 from typing import Any
 from typing import cast
 from typing import Optional
-from typing import Tuple
 from typing import Union
 
 import requests
 import simplejson as jsonlib
 from requests.auth import AuthBase
-from requests.exceptions import HTTPError
 
-from .error import NessieConflictException
-from .error import NessieException
-from .error import NessieNotFoundException
-from .error import NessiePermissionException
-from .error import NessiePreconidtionFailedException
-from .error import NessieServerException
-from .error import NessieUnauthorizedException
+from .error import _create_exception
 
 
 def _get_headers(has_body: bool = False) -> dict:
@@ -27,53 +19,49 @@ def _get_headers(has_body: bool = False) -> dict:
     return headers
 
 
-def _get(url: str, auth: AuthBase, details: str = "", ssl_verify: bool = True, params: dict = None) -> Union[str, dict, list]:
+def _get(url: str, auth: AuthBase, ssl_verify: bool = True, params: dict = None) -> Union[str, dict, list]:
     r = requests.get(url, headers=_get_headers(), verify=ssl_verify, params=params, auth=auth)
-    return _check_error(r, details)
+    return _check_error(r)
 
 
-def _post(
-    url: str, auth: AuthBase, json: Union[str, dict] = None, details: str = "", ssl_verify: bool = True, params: dict = None
-) -> Union[str, dict, list]:
+def _post(url: str, auth: AuthBase, json: Union[str, dict] = None, ssl_verify: bool = True, params: dict = None) -> Union[str, dict, list]:
     if isinstance(json, str):
         json = jsonlib.loads(json)
     r = requests.post(url, headers=_get_headers(json is not None), verify=ssl_verify, json=json, params=params, auth=auth)
-    return _check_error(r, details)
+    return _check_error(r)
 
 
-def _delete(url: str, auth: AuthBase, details: str = "", ssl_verify: bool = True, params: dict = None) -> Union[str, dict, list]:
+def _delete(url: str, auth: AuthBase, ssl_verify: bool = True, params: dict = None) -> Union[str, dict, list]:
     r = requests.delete(url, headers=_get_headers(), verify=ssl_verify, params=params, auth=auth)
-    return _check_error(r, details)
+    return _check_error(r)
 
 
-def _put(url: str, auth: AuthBase, json: Union[str, dict] = None, details: str = "", ssl_verify: bool = True, params: dict = None) -> Any:
+def _put(url: str, auth: AuthBase, json: Union[str, dict] = None, ssl_verify: bool = True, params: dict = None) -> Any:
     if isinstance(json, str):
         json = jsonlib.loads(json)
     r = requests.put(url, headers=_get_headers(json is not None), verify=ssl_verify, json=json, params=params, auth=auth)
-    return _check_error(r, details)
+    return _check_error(r)
 
 
-def _check_error(r: requests.models.Response, details: str = "") -> Union[str, dict, list]:
-    error, code, _ = _raise_for_status(r)
-    if not error:
+def _check_error(r: requests.models.Response) -> Union[dict, list]:
+    if 200 <= r.status_code < 300:
+        return r.json() if r.content else dict()
+
+    if isinstance(r.reason, bytes):
         try:
-            data = r.json()
-            return data
-        except:  # NOQA
-            return r.text
-    if code == 412:
-        raise NessiePreconidtionFailedException("Unable to complete transaction, please retry " + details, error, r)
-    if code == 401:
-        raise NessieUnauthorizedException("Unauthorized on api endpoint " + details, error, r)
-    if code == 403:
-        raise NessiePermissionException("Not permissioned to view entity at " + details, error, r)
-    if code == 404:
-        raise NessieNotFoundException("No entity exists at " + details, error, r)
-    if code == 409:
-        raise NessieConflictException("Entity already exists at " + details, error, r)
-    if code == 500:
-        raise NessieServerException("Server error at " + details, error, r)
-    raise NessieException("unknown error", error, r)
+            reason = r.reason.decode("utf-8")
+        except UnicodeDecodeError:
+            reason = r.reason.decode("iso-8859-1")
+    else:
+        reason = r.reason
+
+    try:
+        parsed_response = r.json()
+    except:  # NOQA
+        # rare/unexpected case when the server responds with a non-JSON payload for an error
+        parsed_response = dict()
+
+    raise _create_exception(parsed_response, r.status_code, reason, r.url)
 
 
 def all_references(base_url: str, auth: AuthBase, ssl_verify: bool = True) -> list:
@@ -261,29 +249,6 @@ def assign_tag(base_url: str, auth: AuthBase, tag: str, assign_to_json: dict, ol
     url = "/trees/tag/{}".format(tag)
     params = {"expectedHash": old_hash}
     _put(base_url + url, auth, assign_to_json, ssl_verify=ssl_verify, params=params)
-
-
-def _raise_for_status(self: requests.models.Response) -> Tuple[Union[HTTPError, None], int, Union[Any, str]]:
-    """Raises stored :class:`HTTPError`, if one occurred. Copy from requests request.raise_for_status()."""
-    http_error_msg = ""
-    if isinstance(self.reason, bytes):
-        try:
-            reason = self.reason.decode("utf-8")
-        except UnicodeDecodeError:
-            reason = self.reason.decode("iso-8859-1")
-    else:
-        reason = self.reason
-
-    if 400 <= self.status_code < 500:
-        http_error_msg = u"%s Client Error: %s for url: %s" % (self.status_code, reason, self.url)
-
-    elif 500 <= self.status_code < 600:
-        http_error_msg = u"%s Server Error: %s for url: %s" % (self.status_code, reason, self.url)
-
-    if http_error_msg:
-        return HTTPError(http_error_msg, response=self), self.status_code, reason
-    else:
-        return None, self.status_code, reason
 
 
 def cherry_pick(
