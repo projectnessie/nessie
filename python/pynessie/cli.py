0570efc166a0385f6f3bd8e099440c2421c73d7d
Omar Al-Safi <omarsmak@gmail.com>
2021-10-28 09:18:00 +0200

    Clean nessie log cli command up (#2520)

--- python/pynessie/cli.py
+++ python/pynessie/cli.py
@@ -186,7 +186,7 @@ def set_head(ctx: ContextObject, head: str, delete: bool) -> None:
 
 
 @cli.command("log")
-@click.option("-r", "--ref", help="branch to list from. If not supplied the default branch from config is used")
+@click.argument("ref", nargs=1, required=False)
 @click.option("-n", "--number", help="number of log entries to return", type=int)
 @click.option("--since", "--after", help="Only include commits newer than specific date, such as '2001-01-01T00:00:00+00:00'")
 @click.option("--until", "--before", help="Only include commits older than specific date, such as '2999-12-30T23:00:00+00:00'")
@@ -201,8 +201,13 @@ def set_head(ctx: ContextObject, head: str, delete: bool) -> None:
     help="Limit commits to a specific committer (this is the logged in user/account who performed the commit). "
     "Supports specifying multiple committers to filter by.",
 )
-@click.argument("revision_range", nargs=1, required=False)
-@click.argument("paths", nargs=-1, type=click.Path(exists=False), required=False)
+@click.option(
+    "-r",
+    "--revision-range",
+    help="Hash to start viewing log from. If of the form '<start_hash>'..'<end_hash>' "
+    "only show log for given range on the particular ref that was provided, the '<end_hash>' "
+    "is inclusive and '<start_hash>' is exclusive.",
+)
 @click.option(
     "--query",
     "--query-expression",
@@ -229,15 +234,31 @@ def log(  # noqa: C901
     author: List[str],
     committer: List[str],
     revision_range: str,
-    paths: Tuple[click.Path],
     query_expression: str,
 ) -> None:
     """Show commit log.
 
-    REVISION_RANGE optional hash to start viewing log from. If of the form <start_hash>..<end_hash> only show log
-    for given range on the particular ref that was provided
+    REF name of branch or tag to use to show the commit logs
 
-    PATHS optional list of paths. If given, only show commits which affected the given paths
+    Examples:
+
+        nessie log -> show commit logs using the configured default branch
+
+        nessie log dev -> show commit logs for 'dev' branch
+
+        nessie log -n 5 dev -> show commit logs for 'dev' branch limited by 5 commits
+
+        nessie log --revision-range 12345678abcdef..12345678efghj dev -> show commit logs in range of hash
+    '12345678abcdef' and '12345678efghj' in 'dev' branch
+
+        nessie log --author nessie.user dev -> show commit logs for user 'nessie.user' in 'dev' branch
+
+        nessie log --query "commit.author == 'nessie_user2' || commit.author == 'non_existing'" dev ->
+    show commit logs using query in 'dev' branch
+
+        nessie log --after "2019-01-01T00:00:00+00:00" --before "2021-01-01T00:00:00+00:00" dev ->
+    show commit logs between "2019-01-01T00:00:00+00:00" and "2021-01-01T00:00:00+00:00" in 'dev' branch
+
     """
     if not ref:
         ref = ctx.nessie.get_default_branch()
@@ -263,14 +284,23 @@ def log(  # noqa: C901
     if ctx.json:
         click.echo(CommitMetaSchema().dumps(log_result, many=True))
     else:
-        click.echo_via_pager(_format_log_result(x) for x in log_result)
+        click.echo_via_pager(_format_log_result(x, ref, index) for index, x in enumerate(log_result))
 
 
-def _format_log_result(x: CommitMeta) -> str:
-    result = click.style("commit {}\n".format(x.hash_), fg="yellow")
+def _format_log_result(x: CommitMeta, ref: str, index: int) -> str:
+    result = _format_commit_log_string(x.hash_, ref, index)
     result += click.style("Author: {}\n".format(x.author))
     result += click.style("Date: {}\n".format(_format_time(x.commitTime)))
     result += click.style("\n\t{}\n\n".format(x.message))
+    return result
+
+
+def _format_commit_log_string(commit_hash: str, ref: str, index: int) -> str:
+    result = click.style("commit {}".format(commit_hash), fg="yellow")
+    if index == 0:
+        result += click.style(" ({})\n".format(ref), fg="green")
+    else:
+        result += "\n"
     return result
 
 
