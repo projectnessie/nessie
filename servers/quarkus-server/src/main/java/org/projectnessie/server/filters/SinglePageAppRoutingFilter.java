6da37fb77e947f819c86fa56b63d86b03719e264
Robert Stupp <snazy@snazy.de>
2021-12-30 13:45:11 +0100

    Add `/content` to single-page-app-routing in Quarkus

--- servers/quarkus-server/src/main/java/org/projectnessie/server/filters/SinglePageAppRoutingFilter.java
+++ servers/quarkus-server/src/main/java/org/projectnessie/server/filters/SinglePageAppRoutingFilter.java
@@ -23,7 +23,7 @@ public class SinglePageAppRoutingFilter {
 
   // maintain list of base paths that should serve root html.
   // must be updated as more UI paths are introduced
-  private static final String[] UI_ROUTES = {"/tree"};
+  private static final String[] UI_ROUTES = {"/tree", "/content", "/notfound"};
 
   @RouteFilter(100)
   void rerouteUiPaths(RoutingContext rc) {
