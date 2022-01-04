9a39d29eb3ee841a46c14978282959e5399061fc
Robert Stupp <snazy@snazy.de>
2022-01-04 15:23:57 +0100

    Drop support for Python 3.6
    Fixes #2991

--- python/setup.py
+++ python/setup.py
@@ -54,7 +54,6 @@ setup(
         "License :: OSI Approved :: Apache Software License",
         "Natural Language :: English",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
