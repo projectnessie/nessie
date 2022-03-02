85266b7a1a89eb9c98157ae5ec65754a4185067a
Eduard Tudenhöfner <etudenhoefner@gmail.com>
2022-03-02 13:22:00 +0100

    Remove Serializable from ContentKey (#3494)
    GC stuff currently does not require this to be Serializable, so we
    should remove it until it's actually required.

--- model/src/main/java/org/projectnessie/model/ContentKey.java
+++ model/src/main/java/org/projectnessie/model/ContentKey.java
@@ -20,7 +20,6 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import java.io.Serializable;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
@@ -39,7 +38,7 @@ import org.projectnessie.model.ImmutableContentKey.Builder;
 @Value.Immutable
 @JsonSerialize(as = ImmutableContentKey.class)
 @JsonDeserialize(as = ImmutableContentKey.class)
-public abstract class ContentKey implements Serializable {
+public abstract class ContentKey {
 
   private static final char ZERO_BYTE = '\u0000';
   private static final String ZERO_BYTE_STRING = Character.toString(ZERO_BYTE);
