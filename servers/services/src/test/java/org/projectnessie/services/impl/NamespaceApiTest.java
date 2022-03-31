5b033b625fa0f59c4e2ff10826ef4eb255ebb60c
Eduard Tudenhoefner <etudenhoefner@gmail.com>
2022-03-31 11:13:03 +0200

    Prevent explicit creation of empty namespaces
    This also includes minor simplifications in Namespace tests for different Web servers

---
+++ servers/services/src/test/java/org/projectnessie/services/impl/NamespaceApiTest.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (C) 2022 Dremio
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.projectnessie.services.impl;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.Test;
+import org.projectnessie.api.params.NamespaceParams;
+import org.projectnessie.model.Namespace;
+
+public class NamespaceApiTest {
+
+  @Test
+  public void emptyNamespaceCreation() {
+    NamespaceApiImpl api = new NamespaceApiImpl(null, null, null, null);
+    assertThatThrownBy(
+            () ->
+                api.createNamespace(
+                    NamespaceParams.builder().refName("main").namespace(Namespace.EMPTY).build()))
+        .isInstanceOf(IllegalArgumentException.class)
+        .hasMessage("Namespace name must not be empty");
+  }
+}
