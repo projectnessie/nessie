9787c25cae8abfc0333609c6afb74a8324672429
Robert Stupp <snazy@snazy.de>
2022-07-01 12:51:50 +0200

    Add build used by integrations-testing for Iceberg dependencies
    Builds on the changes for #4645 and adds a usually unused Gradle
    build in the `iceberg/` directory. This new Gradle build will be
    used by integrations-testing to build the Nessie projects that
    have dependencies to Apache Iceberg.
    
    Tools integrations testing will require a "surrounding" Gradle
    build, which in turn includes three builds:
    * Nessie build, but without all projects that depend on Apache
      Iceberg artifacts. For example: nessie-gc-base, spark-*
    * Apache Iceberg build, which then uses the artifacts of the Nessie
      build.
    * Nessie-Iceberg build containing the Gradle projects that are
      excluded in the first Nessie build, which can that way reference
      both the Nessie and Apache Iceberg artifacts. This includes the
      nessie-gc-base and nessie-spark-* projects.

---
+++ nessie-iceberg/build.gradle.kts
@@ -0,0 +1,37 @@
+/*
+ * Copyright (C) 2022 Dremio
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+plugins {
+  signing
+  `maven-publish`
+  id("org.projectnessie.buildsupport.ide-integration")
+  `nessie-conventions`
+}
+
+// Pull the extra properties from the "main" Nessie build. This ensures that the same versions
+// are being used and we don't need to tweak the dependabot config.
+val extraPropertyPattern = java.util.regex.Pattern.compile("val (version[A-Z][A-Za-z0-9_]+) = \"([0-9a-zA-Z-.]+)\"")
+file("../build.gradle.kts").readLines().forEach { line ->
+  val lineMatch = extraPropertyPattern.matcher(line)
+  if (lineMatch.matches()) {
+    extra[lineMatch.group(1)] = lineMatch.group(2)
+  }
+}
+
+publishingHelper {
+  nessieRepoName.set("nessie")
+  inceptionYear.set("2020")
+}
