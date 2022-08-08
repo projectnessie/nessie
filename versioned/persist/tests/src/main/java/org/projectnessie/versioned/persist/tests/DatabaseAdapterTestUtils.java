b84065647e4d47f1e6f21872fb88b794e4e068f0
Christopher Lambert <xn137@gmx.de>
2022-08-08 14:10:01 +0200

    Add missing methods to adjustable adapter configs (#4889)
    also add test to enforce this convention going forward

--- versioned/persist/tests/src/main/java/org/projectnessie/versioned/persist/tests/DatabaseAdapterTestUtils.java
+++ versioned/persist/tests/src/main/java/org/projectnessie/versioned/persist/tests/DatabaseAdapterTestUtils.java
@@ -15,12 +15,71 @@
  */
 package org.projectnessie.versioned.persist.tests;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.fail;
 
+import java.lang.reflect.Method;
+import java.util.List;
+import java.util.Locale;
+import java.util.Optional;
 import java.util.function.Consumer;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import org.junit.jupiter.api.Assertions;
 import org.projectnessie.versioned.ContentAttachment;
+import org.projectnessie.versioned.persist.adapter.DatabaseAdapterConfig;
 
 public class DatabaseAdapterTestUtils {
+
   public static final Consumer<ContentAttachment> ALWAYS_THROWING_ATTACHMENT_CONSUMER =
       attachment -> fail("Unexpected use of Consumer<ContentAttachment>");
+
+  public static <T extends DatabaseAdapterConfig> void assertAdjustableConfigConvention(
+      Class<T> configClass, Class<? extends T> adjustableConfigClass) {
+    assertThat(adjustableConfigClass).isInterface();
+    assertThat(configClass).isInterface().isAssignableFrom(adjustableConfigClass);
+
+    List<String> missingMethods =
+        Stream.of(configClass.getDeclaredMethods())
+            .filter(method -> method.getName().startsWith("get"))
+            .map(
+                method -> {
+                  String expectedSetterName = method.getName().replaceFirst("get", "with");
+                  String expectedParamName = expectedSetterName.replaceFirst("with", "");
+                  expectedParamName =
+                      expectedParamName.substring(0, 1).toLowerCase(Locale.ROOT)
+                          + expectedParamName.substring(1);
+                  String expectedMethodCode =
+                      adjustableConfigClass.getSimpleName()
+                          + " "
+                          + expectedSetterName
+                          + "("
+                          + method.getReturnType().getSimpleName()
+                          + " "
+                          + expectedParamName
+                          + ");";
+                  try {
+                    Method setter =
+                        adjustableConfigClass.getDeclaredMethod(
+                            expectedSetterName, method.getReturnType());
+                    if (setter.getParameters().length == 1
+                        && setter.getParameters()[0].getName().equals(expectedParamName)) {
+                      return Optional.<String>empty();
+                    }
+                  } catch (NoSuchMethodException e) {
+                    // ignore
+                  }
+                  return Optional.of(expectedMethodCode);
+                })
+            .filter(Optional::isPresent)
+            .map(Optional::get)
+            .collect(Collectors.toList());
+    if (!missingMethods.isEmpty()) {
+      Assertions.fail(
+          "By convention "
+              + adjustableConfigClass.getSimpleName()
+              + " is missing the following methods:\n"
+              + String.join("\n", missingMethods));
+    }
+  }
 }
