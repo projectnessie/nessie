acf84cbd87db71de8f890ff5879fea5ab107d24a
Robert Stupp <snazy@snazy.de>
2022-12-02 20:56:44 +0100

    New storage: RocksDB

---
+++ versioned/storage/rocksdb/src/main/java/org/projectnessie/versioned/storage/rocksdb/RocksDBPersist.java
@@ -0,0 +1,583 @@
+/*
+ * Copyright (C) 2022 Dremio
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.projectnessie.versioned.storage.rocksdb;
+
+import static com.google.common.base.Preconditions.checkArgument;
+import static org.projectnessie.versioned.storage.common.persist.ObjId.EMPTY_OBJ_ID;
+import static org.projectnessie.versioned.storage.common.persist.Reference.reference;
+import static org.projectnessie.versioned.storage.serialize.ProtoSerialization.deserializeObj;
+import static org.projectnessie.versioned.storage.serialize.ProtoSerialization.deserializeObjId;
+import static org.projectnessie.versioned.storage.serialize.ProtoSerialization.deserializeReference;
+import static org.projectnessie.versioned.storage.serialize.ProtoSerialization.serializeObj;
+import static org.projectnessie.versioned.storage.serialize.ProtoSerialization.serializeReference;
+
+import com.google.common.collect.AbstractIterator;
+import com.google.protobuf.ByteString;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.locks.Lock;
+import java.util.function.Predicate;
+import javax.annotation.Nonnull;
+import org.projectnessie.versioned.storage.common.config.StoreConfig;
+import org.projectnessie.versioned.storage.common.exceptions.ObjNotFoundException;
+import org.projectnessie.versioned.storage.common.exceptions.ObjTooLargeException;
+import org.projectnessie.versioned.storage.common.exceptions.RefAlreadyExistsException;
+import org.projectnessie.versioned.storage.common.exceptions.RefConditionFailedException;
+import org.projectnessie.versioned.storage.common.exceptions.RefNotFoundException;
+import org.projectnessie.versioned.storage.common.persist.CloseableIterator;
+import org.projectnessie.versioned.storage.common.persist.Obj;
+import org.projectnessie.versioned.storage.common.persist.ObjId;
+import org.projectnessie.versioned.storage.common.persist.ObjType;
+import org.projectnessie.versioned.storage.common.persist.Persist;
+import org.projectnessie.versioned.storage.common.persist.Reference;
+import org.rocksdb.ColumnFamilyHandle;
+import org.rocksdb.RocksDBException;
+import org.rocksdb.RocksIterator;
+import org.rocksdb.TransactionDB;
+
+class RocksDBPersist implements Persist {
+
+  private final RocksDBBackend backend;
+  private final RocksDBRepo repo;
+  private final StoreConfig config;
+
+  private final ByteString keyPrefix;
+
+  RocksDBPersist(RocksDBBackend backend, RocksDBRepo repo, StoreConfig config) {
+    this.backend = backend;
+    this.repo = repo;
+    this.config = config;
+    this.keyPrefix = ByteString.copyFromUtf8(config.repositoryId() + ':');
+  }
+
+  private byte[] dbKey(ByteString key) {
+    return keyPrefix.concat(key).toByteArray();
+  }
+
+  private byte[] dbKey(String key) {
+    return dbKey(ByteString.copyFromUtf8(key));
+  }
+
+  private byte[] dbKey(ObjId id) {
+    return dbKey(id.asBytes());
+  }
+
+  private RuntimeException rocksDbException(RocksDBException e) {
+    throw new RuntimeException("Unhandled RocksDB exception", e);
+  }
+
+  @Nonnull
+  @jakarta.annotation.Nonnull
+  @Override
+  public String name() {
+    return RocksDBBackendFactory.NAME;
+  }
+
+  @Override
+  public boolean isCaching() {
+    return false;
+  }
+
+  @Override
+  @Nonnull
+  @jakarta.annotation.Nonnull
+  public StoreConfig config() {
+    return config;
+  }
+
+  @Override
+  public Reference findReference(@Nonnull @jakarta.annotation.Nonnull String name) {
+    try {
+      RocksDBBackend v = backend;
+      TransactionDB db = v.db();
+      ColumnFamilyHandle cf = v.refs();
+      byte[] key = dbKey(name);
+
+      byte[] reference = db.get(cf, key);
+      return deserializeReference(reference);
+    } catch (RocksDBException e) {
+      throw rocksDbException(e);
+    }
+  }
+
+  @Override
+  @Nonnull
+  @jakarta.annotation.Nonnull
+  public Reference[] findReferences(@Nonnull @jakarta.annotation.Nonnull String[] names) {
+    try {
+      RocksDBBackend v = backend;
+      TransactionDB db = v.db();
+      ColumnFamilyHandle cf = v.refs();
+
+      int num = names.length;
+      Reference[] r = new Reference[num];
+      List<ColumnFamilyHandle> handles = new ArrayList<>(num);
+      List<byte[]> keys = new ArrayList<>(num);
+      for (String name : names) {
+        if (name != null) {
+          handles.add(cf);
+          keys.add(dbKey(name));
+        }
+      }
+
+      if (!keys.isEmpty()) {
+        List<byte[]> dbResult = db.multiGetAsList(handles, keys);
+
+        for (int i = 0, ri = 0; i < num; i++) {
+          String name = names[i];
+          if (name != null) {
+            byte[] reference = dbResult.get(ri++);
+            r[i] = deserializeReference(reference);
+          }
+        }
+      }
+
+      return r;
+    } catch (RocksDBException e) {
+      throw rocksDbException(e);
+    }
+  }
+
+  @Override
+  @Nonnull
+  @jakarta.annotation.Nonnull
+  public Reference addReference(@Nonnull @jakarta.annotation.Nonnull Reference reference)
+      throws RefAlreadyExistsException {
+    checkArgument(!reference.deleted(), "Deleted references must not be added");
+
+    Lock l = repo.referencesLock(reference.name());
+    try {
+      RocksDBBackend b = backend;
+      TransactionDB db = b.db();
+      ColumnFamilyHandle cf = b.refs();
+      byte[] key = dbKey(reference.name());
+
+      byte[] existing = db.get(cf, key);
+      if (existing != null) {
+        throw new RefAlreadyExistsException(deserializeReference(existing));
+      }
+
+      db.put(cf, key, serializeReference(reference));
+
+      return reference;
+    } catch (RocksDBException e) {
+      throw rocksDbException(e);
+    } finally {
+      l.unlock();
+    }
+  }
+
+  @Override
+  @Nonnull
+  @jakarta.annotation.Nonnull
+  public Reference markReferenceAsDeleted(@Nonnull @jakarta.annotation.Nonnull Reference reference)
+      throws RefNotFoundException, RefConditionFailedException {
+    Lock l = repo.referencesLock(reference.name());
+    try {
+      RocksDBBackend b = backend;
+      TransactionDB db = b.db();
+      ColumnFamilyHandle cf = b.refs();
+      byte[] key = dbKey(reference.name());
+
+      checkReference(reference, db, cf, key, false);
+
+      Reference asDeleted = reference(reference.name(), reference.pointer(), true);
+      db.put(cf, key, serializeReference(asDeleted));
+      return asDeleted;
+    } catch (RocksDBException e) {
+      throw rocksDbException(e);
+    } finally {
+      l.unlock();
+    }
+  }
+
+  private static void checkReference(
+      Reference reference,
+      TransactionDB db,
+      ColumnFamilyHandle cf,
+      byte[] key,
+      boolean expectDeleted)
+      throws RocksDBException, RefNotFoundException, RefConditionFailedException {
+    byte[] existing = db.get(cf, key);
+    if (existing == null) {
+      throw new RefNotFoundException(reference);
+    }
+
+    Reference ref = deserializeReference(existing);
+    if (ref.deleted() != expectDeleted || !ref.pointer().equals(reference.pointer())) {
+      throw new RefConditionFailedException(ref);
+    }
+  }
+
+  @Override
+  public void purgeReference(@Nonnull @jakarta.annotation.Nonnull Reference reference)
+      throws RefNotFoundException, RefConditionFailedException {
+    Lock l = repo.referencesLock(reference.name());
+    try {
+      RocksDBBackend b = backend;
+      TransactionDB db = b.db();
+      ColumnFamilyHandle cf = b.refs();
+      byte[] key = dbKey(reference.name());
+
+      checkReference(reference, db, cf, key, true);
+
+      db.delete(cf, key);
+    } catch (RocksDBException e) {
+      throw rocksDbException(e);
+    } finally {
+      l.unlock();
+    }
+  }
+
+  @Override
+  @Nonnull
+  @jakarta.annotation.Nonnull
+  public Reference updateReferencePointer(
+      @Nonnull @jakarta.annotation.Nonnull Reference reference,
+      @Nonnull @jakarta.annotation.Nonnull ObjId newPointer)
+      throws RefNotFoundException, RefConditionFailedException {
+    Lock l = repo.referencesLock(reference.name());
+    try {
+      RocksDBBackend b = backend;
+      TransactionDB db = b.db();
+      ColumnFamilyHandle cf = b.refs();
+      byte[] key = dbKey(reference.name());
+
+      checkReference(reference, db, cf, key, false);
+
+      Reference updated = reference(reference.name(), newPointer, false);
+
+      db.put(cf, key, serializeReference(updated));
+      return updated;
+    } catch (RocksDBException e) {
+      throw rocksDbException(e);
+    } finally {
+      l.unlock();
+    }
+  }
+
+  @Override
+  public Obj fetchObj(@Nonnull @jakarta.annotation.Nonnull ObjId id) throws ObjNotFoundException {
+    if (EMPTY_OBJ_ID.equals(id)) {
+      return null;
+    }
+
+    try {
+      RocksDBBackend b = backend;
+      TransactionDB db = b.db();
+      ColumnFamilyHandle cf = b.objs();
+      byte[] key = dbKey(id);
+
+      byte[] obj = db.get(cf, key);
+      if (obj == null) {
+        throw new ObjNotFoundException(id);
+      }
+      return deserializeObj(id, obj);
+    } catch (RocksDBException e) {
+      throw rocksDbException(e);
+    }
+  }
+
+  @Override
+  public <T extends Obj> T fetchTypedObj(
+      @Nonnull @jakarta.annotation.Nonnull ObjId id, ObjType type, Class<T> typeClass)
+      throws ObjNotFoundException {
+    Obj obj = fetchObj(id);
+    if (obj == null) {
+      return null;
+    }
+    if (obj.type() != type) {
+      throw new ObjNotFoundException(id);
+    }
+    @SuppressWarnings("unchecked")
+    T r = (T) obj;
+    return r;
+  }
+
+  @Override
+  public ObjType fetchObjType(@Nonnull @jakarta.annotation.Nonnull ObjId id)
+      throws ObjNotFoundException {
+    Obj obj = fetchObj(id);
+    return obj != null ? obj.type() : null;
+  }
+
+  @Override
+  @Nonnull
+  @jakarta.annotation.Nonnull
+  public Obj[] fetchObjs(@Nonnull @jakarta.annotation.Nonnull ObjId[] ids)
+      throws ObjNotFoundException {
+    try {
+      RocksDBBackend b = backend;
+      TransactionDB db = b.db();
+      ColumnFamilyHandle cf = b.objs();
+
+      int num = ids.length;
+      Obj[] r = new Obj[num];
+      List<ColumnFamilyHandle> handles = new ArrayList<>(num);
+      List<byte[]> keys = new ArrayList<>(num);
+      for (ObjId id : ids) {
+        if (id != null && !EMPTY_OBJ_ID.equals(id)) {
+          handles.add(cf);
+          keys.add(dbKey(id));
+        }
+      }
+
+      if (!keys.isEmpty()) {
+        List<ObjId> notFound = null;
+        List<byte[]> dbResult = db.multiGetAsList(handles, keys);
+        for (int i = 0, ri = 0; i < num; i++) {
+          ObjId id = ids[i];
+          if (id != null && !EMPTY_OBJ_ID.equals(id)) {
+            byte[] obj = dbResult.get(ri++);
+            if (obj == null) {
+              if (notFound == null) {
+                notFound = new ArrayList<>();
+              }
+              notFound.add(id);
+            } else {
+              r[i] = deserializeObj(id, obj);
+            }
+          }
+        }
+        if (notFound != null) {
+          throw new ObjNotFoundException(notFound);
+        }
+      }
+
+      return r;
+    } catch (RocksDBException e) {
+      throw rocksDbException(e);
+    }
+  }
+
+  @Override
+  public ObjId storeObj(
+      @Nonnull @jakarta.annotation.Nonnull Obj obj, boolean ignoreSoftSizeRestrictions)
+      throws ObjTooLargeException {
+    checkArgument(
+        obj.id() != null && !EMPTY_OBJ_ID.equals(obj.id()),
+        "Obj to store must have a non-null ID, which must not be %s",
+        EMPTY_OBJ_ID);
+
+    Lock l = repo.objLock(obj.id());
+    try {
+      RocksDBBackend b = backend;
+      TransactionDB db = b.db();
+      ColumnFamilyHandle cf = b.objs();
+      byte[] key = dbKey(obj.id());
+
+      byte[] existing = db.get(cf, key);
+      if (existing != null) {
+        return null;
+      }
+
+      int incrementalIndexSizeLimit =
+          ignoreSoftSizeRestrictions ? Integer.MAX_VALUE : effectiveIncrementalIndexSizeLimit();
+      int indexSizeLimit =
+          ignoreSoftSizeRestrictions ? Integer.MAX_VALUE : effectiveIndexSegmentSizeLimit();
+      byte[] serialized = serializeObj(obj, incrementalIndexSizeLimit, indexSizeLimit);
+
+      db.put(cf, key, serialized);
+      return obj.id();
+    } catch (RocksDBException e) {
+      throw rocksDbException(e);
+    } finally {
+      l.unlock();
+    }
+  }
+
+  @Override
+  @Nonnull
+  @jakarta.annotation.Nonnull
+  public ObjId[] storeObjs(@Nonnull @jakarta.annotation.Nonnull Obj[] objs)
+      throws ObjTooLargeException {
+    ObjId[] r = new ObjId[objs.length];
+    for (int i = 0; i < objs.length; i++) {
+      Obj o = objs[i];
+      if (o != null) {
+        r[i] = storeObj(o, false);
+      }
+    }
+    return r;
+  }
+
+  @Override
+  public void deleteObj(@Nonnull @jakarta.annotation.Nonnull ObjId id) {
+    Lock l = repo.objLock(id);
+    try {
+      RocksDBBackend b = backend;
+      TransactionDB db = b.db();
+      ColumnFamilyHandle cf = b.objs();
+      byte[] key = dbKey(id);
+
+      db.delete(cf, key);
+    } catch (RocksDBException e) {
+      throw rocksDbException(e);
+    } finally {
+      l.unlock();
+    }
+  }
+
+  @Override
+  public void deleteObjs(@Nonnull @jakarta.annotation.Nonnull ObjId[] ids) {
+    for (ObjId id : ids) {
+      deleteObj(id);
+    }
+  }
+
+  @Override
+  public void updateObj(@Nonnull @jakarta.annotation.Nonnull Obj obj)
+      throws ObjTooLargeException, ObjNotFoundException {
+    ObjId id = obj.id();
+    checkArgument(
+        id != null && !EMPTY_OBJ_ID.equals(id),
+        "Obj to store must have a non-null ID, which must not be %s",
+        EMPTY_OBJ_ID);
+
+    Lock l = repo.objLock(obj.id());
+    try {
+      RocksDBBackend b = backend;
+      TransactionDB db = b.db();
+      ColumnFamilyHandle cf = b.objs();
+      byte[] key = dbKey(id);
+
+      byte[] existing = db.get(cf, key);
+      if (existing == null || deserializeObj(id, existing).type() != obj.type()) {
+        throw new ObjNotFoundException(id);
+      }
+
+      byte[] serialized = serializeObj(obj, Integer.MAX_VALUE, Integer.MAX_VALUE);
+
+      db.put(cf, key, serialized);
+    } catch (RocksDBException e) {
+      throw rocksDbException(e);
+    } finally {
+      l.unlock();
+    }
+  }
+
+  @Override
+  public void updateObjs(@Nonnull @jakarta.annotation.Nonnull Obj[] objs)
+      throws ObjTooLargeException, ObjNotFoundException {
+    for (Obj obj : objs) {
+      updateObj(obj);
+    }
+  }
+
+  @Override
+  public void erase() {
+    // erase() does not use any lock, it's use is rare, taking the risk of having a corrupted,
+    // erased repo
+
+    RocksDBBackend b = backend;
+    TransactionDB db = b.db();
+
+    b.all()
+        .forEach(
+            cf -> {
+              try (RocksIterator iter = db.newIterator(cf)) {
+                List<ByteString> deletes = new ArrayList<>();
+                for (iter.seekToFirst(); iter.isValid(); iter.next()) {
+                  ByteString key = ByteString.copyFrom(iter.key());
+                  if (key.startsWith(keyPrefix)) {
+                    deletes.add(key);
+                  }
+                }
+                deletes.forEach(
+                    key -> {
+                      try {
+                        db.delete(cf, key.toByteArray());
+                      } catch (RocksDBException e) {
+                        throw rocksDbException(e);
+                      }
+                    });
+              }
+            });
+  }
+
+  @Nonnull
+  @jakarta.annotation.Nonnull
+  @Override
+  public CloseableIterator<Obj> scanAllObjects(
+      @Nonnull @jakarta.annotation.Nonnull Set<ObjType> returnedObjTypes) {
+    return new ScanAllObjectsIterator(returnedObjTypes::contains);
+  }
+
+  private class ScanAllObjectsIterator extends AbstractIterator<Obj>
+      implements CloseableIterator<Obj> {
+
+    private final Predicate<ObjType> filter;
+
+    private final TransactionDB db;
+    private final ColumnFamilyHandle cf;
+    private final RocksIterator iter;
+    private boolean first = true;
+
+    ScanAllObjectsIterator(Predicate<ObjType> filter) {
+      this.filter = filter;
+
+      RocksDBBackend b = backend;
+      db = b.db();
+      cf = b.objs();
+      iter = db.newIterator(b.objs());
+      iter.seekToFirst();
+    }
+
+    @Override
+    protected Obj computeNext() {
+      while (true) {
+        if (!iter.isValid()) {
+          return endOfData();
+        }
+
+        if (first) {
+          first = false;
+        } else {
+          iter.next();
+        }
+
+        byte[] k = iter.key();
+        ByteString key = ByteString.copyFrom(k);
+        if (!key.startsWith(keyPrefix)) {
+          continue;
+        }
+
+        byte[] obj;
+        try {
+          obj = db.get(cf, k);
+        } catch (RocksDBException e) {
+          throw rocksDbException(e);
+        }
+        if (obj == null) {
+          continue;
+        }
+
+        ObjId id = deserializeObjId(key.substring(keyPrefix.size()));
+        Obj o = deserializeObj(id, obj);
+
+        if (filter.test(o.type())) {
+          return o;
+        }
+      }
+    }
+
+    @Override
+    public void close() {
+      iter.close();
+    }
+  }
+}
---
+++ versioned/storage/rocksdb/src/test/java/org/projectnessie/versioned/storage/rocksdb/TestRocksDBBackendFactory.java
@@ -0,0 +1,138 @@
+/*
+ * Copyright (C) 2022 Dremio
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.projectnessie.versioned.storage.rocksdb;
+
+import static org.projectnessie.versioned.storage.common.logic.Logics.setupLogic;
+
+import java.nio.file.Path;
+import org.assertj.core.api.SoftAssertions;
+import org.assertj.core.api.junit.jupiter.InjectSoftAssertions;
+import org.assertj.core.api.junit.jupiter.SoftAssertionsExtension;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.io.TempDir;
+import org.projectnessie.versioned.storage.common.config.StoreConfig;
+import org.projectnessie.versioned.storage.common.logic.RepositoryDescription;
+import org.projectnessie.versioned.storage.common.logic.SetupLogic;
+import org.projectnessie.versioned.storage.common.persist.Backend;
+import org.projectnessie.versioned.storage.common.persist.BackendFactory;
+import org.projectnessie.versioned.storage.common.persist.Persist;
+import org.projectnessie.versioned.storage.common.persist.PersistFactory;
+import org.projectnessie.versioned.storage.common.persist.PersistLoader;
+
+@ExtendWith(SoftAssertionsExtension.class)
+public class TestRocksDBBackendFactory {
+  @InjectSoftAssertions protected SoftAssertions soft;
+
+  @TempDir protected Path rocksDir;
+
+  static StoreConfig DEFAULT_CONFIG = new StoreConfig() {};
+
+  @Test
+  public void productionLike() throws Exception {
+    BackendFactory<RocksDBBackendConfig> factory =
+        PersistLoader.findFactoryByName(RocksDBBackendFactory.NAME);
+    soft.assertThat(factory).isNotNull().isInstanceOf(RocksDBBackendFactory.class);
+
+    RepositoryDescription repoDesc;
+
+    try (Backend backend =
+        factory.buildBackend(RocksDBBackendConfig.builder().databasePath(rocksDir).build())) {
+      soft.assertThat(backend).isNotNull().isInstanceOf(RocksDBBackend.class);
+      backend.setupSchema();
+      PersistFactory persistFactory = backend.createFactory();
+      soft.assertThat(persistFactory).isNotNull().isInstanceOf(RocksDBPersistFactory.class);
+      Persist persist = persistFactory.newPersist(DEFAULT_CONFIG);
+      soft.assertThat(persist).isNotNull().isInstanceOf(RocksDBPersist.class);
+
+      SetupLogic setupLogic = setupLogic(persist);
+      setupLogic.initialize("initializeAgain");
+      repoDesc = setupLogic.fetchRepositoryDescription();
+      soft.assertThat(repoDesc).isNotNull();
+    }
+
+    try (Backend backend =
+        factory.buildBackend(RocksDBBackendConfig.builder().databasePath(rocksDir).build())) {
+      soft.assertThat(backend).isNotNull().isInstanceOf(RocksDBBackend.class);
+      backend.setupSchema();
+      PersistFactory persistFactory = backend.createFactory();
+      soft.assertThat(persistFactory).isNotNull().isInstanceOf(RocksDBPersistFactory.class);
+      Persist persist = persistFactory.newPersist(DEFAULT_CONFIG);
+      soft.assertThat(persist).isNotNull().isInstanceOf(RocksDBPersist.class);
+
+      SetupLogic setupLogic = setupLogic(persist);
+      setupLogic.initialize("initializeAgain");
+      soft.assertThat(setupLogic.fetchRepositoryDescription()).isEqualTo(repoDesc);
+    }
+  }
+
+  @Test
+  public void testFactory() throws Exception {
+    RocksDBBackendTestFactory testFactory = new RocksDBBackendTestFactory();
+
+    RepositoryDescription repoDesc;
+
+    testFactory.start();
+    try {
+      try (Backend backend = testFactory.createNewBackend()) {
+        soft.assertThat(backend).isNotNull().isInstanceOf(RocksDBBackend.class);
+        backend.setupSchema();
+        PersistFactory persistFactory = backend.createFactory();
+        soft.assertThat(persistFactory).isNotNull().isInstanceOf(RocksDBPersistFactory.class);
+        Persist persist = persistFactory.newPersist(DEFAULT_CONFIG);
+        soft.assertThat(persist).isNotNull().isInstanceOf(RocksDBPersist.class);
+
+        SetupLogic setupLogic = setupLogic(persist);
+        setupLogic.initialize("initializeAgain");
+        repoDesc = setupLogic.fetchRepositoryDescription();
+        soft.assertThat(repoDesc).isNotNull();
+      }
+
+      try (Backend backend = testFactory.createNewBackend()) {
+        soft.assertThat(backend).isNotNull().isInstanceOf(RocksDBBackend.class);
+        backend.setupSchema();
+        PersistFactory persistFactory = backend.createFactory();
+        soft.assertThat(persistFactory).isNotNull().isInstanceOf(RocksDBPersistFactory.class);
+        Persist persist = persistFactory.newPersist(DEFAULT_CONFIG);
+        soft.assertThat(persist).isNotNull().isInstanceOf(RocksDBPersist.class);
+
+        SetupLogic setupLogic = setupLogic(persist);
+        setupLogic.initialize("initializeAgain");
+        soft.assertThat(setupLogic.fetchRepositoryDescription()).isEqualTo(repoDesc);
+      }
+    } finally {
+      testFactory.stop();
+    }
+
+    testFactory.start();
+    try {
+      try (Backend backend = testFactory.createNewBackend()) {
+        soft.assertThat(backend).isNotNull().isInstanceOf(RocksDBBackend.class);
+        backend.setupSchema();
+        PersistFactory persistFactory = backend.createFactory();
+        soft.assertThat(persistFactory).isNotNull().isInstanceOf(RocksDBPersistFactory.class);
+        Persist persist = persistFactory.newPersist(DEFAULT_CONFIG);
+        soft.assertThat(persist).isNotNull().isInstanceOf(RocksDBPersist.class);
+
+        SetupLogic setupLogic = setupLogic(persist);
+        setupLogic.initialize("initializeAgain");
+        soft.assertThat(setupLogic.fetchRepositoryDescription()).isNotEqualTo(repoDesc);
+      }
+    } finally {
+      testFactory.stop();
+    }
+  }
+}
