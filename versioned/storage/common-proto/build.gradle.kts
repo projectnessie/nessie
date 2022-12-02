9da82160212f4ab1a6d6f7573ae155780cdcd17b
Robert Stupp <snazy@snazy.de>
2022-12-02 21:09:26 +0100

    New storage: protobuf serialization

---
+++ versioned/storage/common-proto/build.gradle.kts
@@ -0,0 +1,69 @@
+/*
+ * Copyright (C) 2022 Dremio
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import com.google.protobuf.gradle.ProtobufExtension
+
+plugins {
+  `java-library`
+  `maven-publish`
+  signing
+  id("org.projectnessie.buildsupport.reflectionconfig")
+  `nessie-conventions`
+}
+
+apply<ProtobufHelperPlugin>()
+
+extra["maven.name"] = "Nessie - Storage - Common proto"
+
+description = "Protobuf definition for the serialization for storage objects."
+
+dependencies { implementation(libs.protobuf.java) }
+
+// *.proto files taken from https://github.com/googleapis/googleapis/ repo, available as a git
+// submodule
+extensions.configure<ProtobufExtension> {
+  // Configure the protoc executable
+  protoc {
+    // Download from repositories
+    artifact = "com.google.protobuf:protoc:${libs.versions.protobuf.get()}"
+  }
+}
+
+reflectionConfig {
+  // Consider classes that extend one of these classes...
+  classExtendsPatterns.set(
+    listOf(
+      "com.google.protobuf.GeneratedMessageV3",
+      "com.google.protobuf.GeneratedMessageV3.Builder"
+    )
+  )
+  // ... and classes the implement this interface.
+  classImplementsPatterns.set(listOf("com.google.protobuf.ProtocolMessageEnum"))
+  // Also include generated classes (e.g. google.protobuf.Empty) via the "runtimeClasspath",
+  // which contains the the "com.google.protobuf:protobuf-java" dependency.
+  includeConfigurations.set(listOf("runtimeClasspath"))
+}
+
+// The protobuf-plugin should ideally do this
+tasks.named<Jar>("sourcesJar") {
+  dependsOn(tasks.named("generateProto"), tasks.named("generateReflectionConfig"))
+}
+
+tasks.withType(com.google.protobuf.gradle.ProtobufExtract::class).configureEach {
+  if (name == "extractIncludeTestProto") {
+    dependsOn(tasks.named("processJandexIndex"))
+  }
+}
