15bd45f66e00cfe9a0940bb9f17ce0f16dd23ce2
Dmitri Bourlatchkov <dmitri.bourlatchkov@dremio.com>
2023-01-10 19:18:13 -0500

    Add QuarkusPrincipalSupplier

---
+++ servers/quarkus-server/src/main/java/org/projectnessie/server/authn/QuarkusPrincipalSupplier.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright (C) 2023 Dremio
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.projectnessie.server.authn;
+
+import io.quarkus.security.identity.SecurityIdentity;
+import java.security.Principal;
+import javax.annotation.Priority;
+import javax.enterprise.context.RequestScoped;
+import javax.enterprise.inject.Alternative;
+import javax.inject.Inject;
+import org.projectnessie.services.rest.PrincipalSupplier;
+
+@RequestScoped
+@Alternative
+@Priority(1)
+public class QuarkusPrincipalSupplier extends PrincipalSupplier {
+  private final SecurityIdentity identity;
+
+  @Inject
+  public QuarkusPrincipalSupplier(SecurityIdentity identity) {
+    this.identity = identity;
+  }
+
+  @Override
+  public Principal get() {
+    return identity.getPrincipal();
+  }
+}
