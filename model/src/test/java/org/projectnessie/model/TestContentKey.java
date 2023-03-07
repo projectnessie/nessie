3b1b30c19e2b42d678454b4629c04423d214bef7
Robert Stupp <snazy@snazy.de>
2023-03-07 13:19:08 +0100

    Replace `Key` with `ContentKey`
    The `Key` class is an unnecessary exact copy of `ContentKey` and can be safely replaced w/ `ContentKey`.

--- model/src/test/java/org/projectnessie/model/TestContentKey.java
+++ model/src/test/java/org/projectnessie/model/TestContentKey.java
@@ -16,25 +16,115 @@
 package org.projectnessie.model;
 
 import static java.util.Arrays.asList;
+import static java.util.Collections.emptyList;
 import static java.util.Collections.singletonList;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.InstanceOfAssertFactories.INTEGER;
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.IOException;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.parallel.Execution;
-import org.junit.jupiter.api.parallel.ExecutionMode;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
-@Execution(ExecutionMode.CONCURRENT)
 class TestContentKey {
+
+  static final String STRING_100 =
+      "1234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890";
+
+  @ParameterizedTest
+  @MethodSource("keyLengthGood")
+  void keyLengthGood(List<String> elements) {
+    ContentKey.of(elements);
+  }
+
+  static Stream<Arguments> keyLengthGood() {
+    return Stream.of(
+        arguments(emptyList()),
+        arguments(singletonList("a")),
+        arguments(singletonList(STRING_100)),
+        arguments(singletonList(STRING_100 + STRING_100 + STRING_100 + STRING_100 + STRING_100)),
+        arguments(asList(STRING_100, STRING_100)),
+        arguments(asList(STRING_100, STRING_100, STRING_100)),
+        arguments(asList(STRING_100, STRING_100, STRING_100, STRING_100)),
+        arguments(asList(STRING_100, STRING_100, STRING_100, STRING_100, STRING_100)));
+  }
+
+  @ParameterizedTest
+  @MethodSource("keyTooLong")
+  void keyTooLong(List<String> elements) {
+    assertThatThrownBy(() -> ContentKey.of(elements))
+        .isInstanceOf(IllegalStateException.class)
+        .hasMessage("Key too long, max allowed length: " + ContentKey.MAX_LENGTH);
+  }
+
+  static Stream<Arguments> keyTooLong() {
+    return Stream.of(
+        arguments(
+            singletonList(STRING_100 + STRING_100 + STRING_100 + STRING_100 + STRING_100 + "x")),
+        arguments(
+            singletonList(
+                STRING_100 + STRING_100 + STRING_100 + STRING_100 + STRING_100 + STRING_100)),
+        arguments(asList(STRING_100, STRING_100, STRING_100, STRING_100, STRING_100, "x")),
+        arguments(asList("x", STRING_100, STRING_100, STRING_100, STRING_100, STRING_100)));
+  }
+
+  @ParameterizedTest
+  @ValueSource(ints = {0, 2, 5, 15, 19, 20})
+  void keyElementsGood(int elements) {
+    ContentKey.of(IntStream.range(0, elements).mapToObj(i -> "foo").collect(Collectors.toList()));
+  }
+
+  @ParameterizedTest
+  @ValueSource(ints = {21, 55, 100})
+  void keyTooManyElements(int elements) {
+    assertThatThrownBy(
+            () ->
+                ContentKey.of(
+                    IntStream.range(0, elements).mapToObj(i -> "foo").collect(Collectors.toList())))
+        .isInstanceOf(IllegalStateException.class)
+        .hasMessage("Key too long, max allowed number of elements: " + ContentKey.MAX_ELEMENTS);
+  }
+
+  @ParameterizedTest
+  @MethodSource("compare")
+  void compare(ContentKey a, ContentKey b, int expectedCompare) {
+    assertThat(a)
+        .describedAs("Compare of %s to %s expect %d", a, b, expectedCompare)
+        .extracting(k -> Integer.signum(k.compareTo(b)))
+        .asInstanceOf(INTEGER)
+        .isEqualTo(expectedCompare);
+  }
+
+  static Stream<Arguments> compare() {
+    return Stream.of(
+        arguments(ContentKey.of("a"), ContentKey.of("a"), 0),
+        arguments(ContentKey.of("a"), ContentKey.of("a", "a"), -1),
+        arguments(ContentKey.of("a", "a"), ContentKey.of("a"), 1),
+        arguments(ContentKey.of("a"), ContentKey.of("abcdef"), -1),
+        arguments(ContentKey.of("abcdef"), ContentKey.of(), 1),
+        arguments(ContentKey.of("abcdef"), ContentKey.of("0123", "123", "123"), 1),
+        arguments(ContentKey.of("abcdef", "abc", "abc"), ContentKey.of("0123"), 1),
+        arguments(ContentKey.of("0"), ContentKey.of("0123", "123", "123"), -1),
+        arguments(ContentKey.of("abcdef", "abc", "abc"), ContentKey.of(), 1),
+        arguments(ContentKey.of("key.0"), ContentKey.of("key.1"), -1),
+        arguments(ContentKey.of("key.1"), ContentKey.of("key.0"), 1),
+        arguments(ContentKey.of("key.42"), ContentKey.of("key.42"), 0),
+        arguments(ContentKey.of("key", "0"), ContentKey.of("key", "1"), -1),
+        arguments(ContentKey.of("key", "1"), ContentKey.of("key", "0"), 1),
+        arguments(ContentKey.of("key", "42"), ContentKey.of("key", "42"), 0));
+  }
 
   @ParameterizedTest
   @MethodSource("contentKeyOfAndParseCases")
