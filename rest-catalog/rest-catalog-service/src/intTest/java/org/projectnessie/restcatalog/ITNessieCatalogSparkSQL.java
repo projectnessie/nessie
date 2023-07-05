f78eaf4e32c4c33eded0c13b547bddd81727e3f4
Alexandre Dutra <adutra@users.noreply.github.com>
2023-07-05 17:26:21 +0200

    Move ITNessieCatalogSparkSQL to right package (#7185)

--- rest-catalog/rest-catalog-service/src/intTest/java/org/projectnessie/restcatalog/ITNessieCatalogSparkSQL.java
+++ rest-catalog/rest-catalog-service/src/intTest/java/org/projectnessie/restcatalog/ITNessieCatalogSparkSQL.java
@@ -13,6 +13,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package org.projectnessie.restcatalog;
+
 import static org.projectnessie.jaxrs.ext.NessieJaxRsExtension.jaxRsExtension;
 
 import java.net.URI;
