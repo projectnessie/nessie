37b23d34f8b7ab519293c3d69df2c6e4f19fee22
Christopher Lambert <xn137@gmx.de>
2023-08-24 18:43:39 +0200

    Add Value.Check to Entry + KeyEntry
    adjust tests accordingly that were using a payload with different
    contentId than the key objects

--- versioned/spi/src/main/java/org/projectnessie/versioned/KeyEntry.java
+++ versioned/spi/src/main/java/org/projectnessie/versioned/KeyEntry.java
@@ -15,6 +15,7 @@
  */
 package org.projectnessie.versioned;
 
+import com.google.common.base.Preconditions;
 import javax.annotation.Nullable;
 import javax.validation.constraints.NotNull;
 import org.immutables.value.Value;
@@ -24,22 +25,39 @@ import org.projectnessie.model.IdentifiedContentKey;
 @Value.Immutable
 public interface KeyEntry {
 
+  @Value.Parameter(order = 1)
   IdentifiedContentKey getKey();
 
   @Nullable
   @jakarta.annotation.Nullable
+  @Value.Parameter(order = 2)
   Content getContent();
 
-  static ImmutableKeyEntry.Builder builder() {
-    return ImmutableKeyEntry.builder();
+  @Value.Check
+  default void verify() {
+    Content content = getContent();
+    if (content == null) {
+      return;
+    }
+    IdentifiedContentKey key = getKey();
+    Preconditions.checkArgument(
+        key.type().equals(content.getType()),
+        "Content type from key '%s' does not match actual type of content: %s",
+        key.type(),
+        content);
+    Preconditions.checkArgument(
+        key.lastElement().contentId().equals(content.getId()),
+        "Content id from key '%s' does not match actual id of content: %s",
+        key,
+        content);
   }
 
   static KeyEntry of(IdentifiedContentKey key) {
-    return builder().key(key).build();
+    return ImmutableKeyEntry.of(key, null);
   }
 
   static KeyEntry of(
       IdentifiedContentKey key, @NotNull @jakarta.validation.constraints.NotNull Content content) {
-    return builder().key(key).content(content).build();
+    return ImmutableKeyEntry.of(key, content);
   }
 }
