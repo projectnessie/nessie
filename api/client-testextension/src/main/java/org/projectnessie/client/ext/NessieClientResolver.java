252772486103774d2a5631a0fda51c04dee0ed0b
Dmitri Bourlatchkov <dmitri.bourlatchkov@dremio.com>
2023-08-28 23:38:34 -0400

    Process outer test instances in clientFactoryForContext
    Make NessieClientResolver.clientFactoryForContext() invoke configuration
    callbacks on all test instances (inner as well as outer).

--- api/client-testextension/src/main/java/org/projectnessie/client/ext/NessieClientResolver.java
+++ api/client-testextension/src/main/java/org/projectnessie/client/ext/NessieClientResolver.java
@@ -19,11 +19,11 @@ import static org.projectnessie.client.ext.MultiVersionApiTest.apiVersion;
 
 import java.io.Serializable;
 import java.net.URI;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
 import javax.annotation.Nonnull;
-import org.junit.jupiter.api.extension.ExtensionContext;
-import org.junit.jupiter.api.extension.ParameterContext;
-import org.junit.jupiter.api.extension.ParameterResolutionException;
-import org.junit.jupiter.api.extension.ParameterResolver;
+import org.junit.jupiter.api.extension.*;
 import org.junit.platform.commons.util.AnnotationUtils;
 import org.projectnessie.client.NessieClientBuilder;
 import org.projectnessie.client.api.NessieApiV1;
@@ -88,7 +88,16 @@ public abstract class NessieClientResolver implements ParameterResolver {
   private NessieClientFactory clientFactoryForContext(ExtensionContext extensionContext) {
     NessieApiVersion apiVersion = apiVersion(extensionContext);
     URI uri = resolvedNessieUri(extensionContext);
-    Object testInstance = extensionContext.getTestInstance().orElse(null);
+    List<NessieClientCustomizer> customizers =
+        extensionContext
+            .getTestInstances()
+            .map(
+                i ->
+                    i.getAllInstances().stream()
+                        .filter(ti -> ti instanceof NessieClientCustomizer)
+                        .map(ti -> (NessieClientCustomizer) ti)
+                        .collect(Collectors.toList()))
+            .orElse(Collections.emptyList());
 
     Class<? extends HttpResponseFactory> responseFactoryClass =
         extensionContext
@@ -97,8 +106,15 @@ public abstract class NessieClientResolver implements ParameterResolver {
             .map(NessieClientResponseFactory::value)
             .orElse(null);
 
-    if (testInstance instanceof NessieClientCustomizer) {
-      NessieClientCustomizer testCustomizer = (NessieClientCustomizer) testInstance;
+    if (!customizers.isEmpty()) {
+      NessieClientCustomizer testCustomizer =
+          (builder, version) -> {
+            for (NessieClientCustomizer customizer : customizers) {
+              builder = customizer.configure(builder, version);
+            }
+            return builder;
+          };
+
       return new ClientFactory(uri, apiVersion, responseFactoryClass) {
         @Nonnull
         @jakarta.annotation.Nonnull
