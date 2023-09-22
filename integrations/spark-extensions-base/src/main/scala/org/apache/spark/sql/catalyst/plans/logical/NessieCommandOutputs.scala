11a11a33519e9b72994c086f2a78256541d100f2
Robert Stupp <snazy@snazy.de>
2023-09-22 12:46:25 +0200

    Spark 3.5 removed the `toAttributes`

--- integrations/spark-extensions-base/src/main/scala/org/apache/spark/sql/catalyst/plans/logical/NessieCommandOutputs.scala
+++ integrations/spark-extensions-base/src/main/scala/org/apache/spark/sql/catalyst/plans/logical/NessieCommandOutputs.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2022 Dremio
+ * Copyright (C) 2023 Dremio
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -19,108 +19,121 @@ import org.apache.spark.sql.catalyst.expressions.AttributeReference
 import org.apache.spark.sql.types.{DataTypes, Metadata, StructField, StructType}
 
 object NessieCommandOutputs {
-  def referenceOutput(): Seq[AttributeReference] = new StructType(
-    Array[StructField](
-      StructField(
-        "refType",
-        DataTypes.StringType,
-        nullable = false,
-        Metadata.empty
-      ),
-      StructField(
-        "name",
-        DataTypes.StringType,
-        nullable = false,
-        Metadata.empty
-      ),
-      StructField(
-        "hash",
-        DataTypes.StringType,
-        nullable = false,
-        Metadata.empty
-      )
+  private def structToAttributes(struct: StructType): Seq[AttributeReference] =
+    struct.map(f =>
+      AttributeReference(f.name, f.dataType, f.nullable, f.metadata)()
     )
-  ).toAttributes
 
-  def simpleReferenceOutput(): Seq[AttributeReference] = new StructType(
-    Array[StructField](
-      StructField(
-        "name",
-        DataTypes.StringType,
-        nullable = false,
-        Metadata.empty
-      ),
-      StructField(
-        "hash",
-        DataTypes.StringType,
-        nullable = false,
-        Metadata.empty
+  def referenceOutput(): Seq[AttributeReference] = structToAttributes(
+    new StructType(
+      Array[StructField](
+        StructField(
+          "refType",
+          DataTypes.StringType,
+          nullable = false,
+          Metadata.empty
+        ),
+        StructField(
+          "name",
+          DataTypes.StringType,
+          nullable = false,
+          Metadata.empty
+        ),
+        StructField(
+          "hash",
+          DataTypes.StringType,
+          nullable = false,
+          Metadata.empty
+        )
       )
     )
-  ).toAttributes
+  )
 
-  def dropReferenceOutput(): Seq[AttributeReference] = new StructType(
-    Array[StructField](
-      StructField(
-        "status",
-        DataTypes.StringType,
-        nullable = false,
-        Metadata.empty
+  def simpleReferenceOutput(): Seq[AttributeReference] = structToAttributes(
+    new StructType(
+      Array[StructField](
+        StructField(
+          "name",
+          DataTypes.StringType,
+          nullable = false,
+          Metadata.empty
+        ),
+        StructField(
+          "hash",
+          DataTypes.StringType,
+          nullable = false,
+          Metadata.empty
+        )
       )
     )
-  ).toAttributes
+  )
 
-  def showLogOutput(): Seq[AttributeReference] = new StructType(
-    Array[StructField](
-      StructField(
-        "author",
-        DataTypes.StringType,
-        nullable = false,
-        Metadata.empty
-      ),
-      StructField(
-        "committer",
-        DataTypes.StringType,
-        nullable = false,
-        Metadata.empty
-      ),
-      StructField(
-        "hash",
-        DataTypes.StringType,
-        nullable = false,
-        Metadata.empty
-      ),
-      StructField(
-        "message",
-        DataTypes.StringType,
-        nullable = false,
-        Metadata.empty
-      ),
-      StructField(
-        "signedOffBy",
-        DataTypes.StringType,
-        nullable = false,
-        Metadata.empty
-      ),
-      StructField(
-        "authorTime",
-        DataTypes.TimestampType,
-        nullable = false,
-        Metadata.empty
-      ),
-      StructField(
-        "committerTime",
-        DataTypes.TimestampType,
-        nullable = false,
-        Metadata.empty
-      ),
-      StructField(
-        "properties",
-        DataTypes
-          .createMapType(DataTypes.StringType, DataTypes.StringType, false),
-        nullable = false,
-        Metadata.empty
+  def dropReferenceOutput(): Seq[AttributeReference] = structToAttributes(
+    new StructType(
+      Array[StructField](
+        StructField(
+          "status",
+          DataTypes.StringType,
+          nullable = false,
+          Metadata.empty
+        )
       )
     )
-  ).toAttributes
+  )
+
+  def showLogOutput(): Seq[AttributeReference] = structToAttributes(
+    new StructType(
+      Array[StructField](
+        StructField(
+          "author",
+          DataTypes.StringType,
+          nullable = false,
+          Metadata.empty
+        ),
+        StructField(
+          "committer",
+          DataTypes.StringType,
+          nullable = false,
+          Metadata.empty
+        ),
+        StructField(
+          "hash",
+          DataTypes.StringType,
+          nullable = false,
+          Metadata.empty
+        ),
+        StructField(
+          "message",
+          DataTypes.StringType,
+          nullable = false,
+          Metadata.empty
+        ),
+        StructField(
+          "signedOffBy",
+          DataTypes.StringType,
+          nullable = false,
+          Metadata.empty
+        ),
+        StructField(
+          "authorTime",
+          DataTypes.TimestampType,
+          nullable = false,
+          Metadata.empty
+        ),
+        StructField(
+          "committerTime",
+          DataTypes.TimestampType,
+          nullable = false,
+          Metadata.empty
+        ),
+        StructField(
+          "properties",
+          DataTypes
+            .createMapType(DataTypes.StringType, DataTypes.StringType, false),
+          nullable = false,
+          Metadata.empty
+        )
+      )
+    )
+  )
 }
