040611503af3a88a14823c5ce1ab24f817cfaa00
Alexandre Dutra <adutra@users.noreply.github.com>
2024-01-29 14:59:48 +0100

    Disable OpenTelemetry SDK when no endpoint is defined
    This change introduces a conditional enablement of OpenTelemetry SDK,
    depending on whether a collector endpoint has been explicitly provided
    by the user or not.
    
    This is inspired by #8000, but it's trickier to solve for one main
    reason: the 3 runtime properties `quarkus.otel.sdk.disabled`,
    `quarkus.otel.exporter.otlp.endpoint` and
    `quarkus.otel.exporter.otlp.traces.endpoint` have default values set by
    `DefaultValuesConfigSource`.
    
    So the best we can do is: detect whether those properties have been
    overwritten or not. If they were, then leave OpenTelemetry enabled; if
    not, disable OpenTelemetry.
    
    This could have one minor impact: users willing to use OpenTelemetry,
    but relying on the default value of
    `quarkus.otel.exporter.otlp.traces.endpoint` (which is
    `http://localhost:4317`) now would need to re-define it for
    OpenTelemetry to stay enabled. I am not sure there are any users in this
    situation though, since the docs already ask users to define this
    property explicitly.
    
    Helm chart users are not affected, since the chart already redefines the
    endpoint property.
    
    Incidentally, this allows to get rid of the SEVERE warning messages when
    the user doesn't want OpenTelemetry enabled.

---
+++ servers/quarkus-common/src/main/java/org/projectnessie/quarkus/config/OpenTelemetryConfigSourceInterceptor.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright (C) 2024 Dremio
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.projectnessie.quarkus.config;
+
+import io.smallrye.config.ConfigSourceInterceptor;
+import io.smallrye.config.ConfigSourceInterceptorContext;
+import io.smallrye.config.ConfigValue;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class OpenTelemetryConfigSourceInterceptor implements ConfigSourceInterceptor {
+
+  private static final Logger LOGGER =
+      LoggerFactory.getLogger(OpenTelemetryConfigSourceInterceptor.class);
+  private static final int APPLICATION_PROPERTIES_CLASSPATH_ORDINAL = 250;
+
+  @Override
+  public ConfigValue getValue(ConfigSourceInterceptorContext context, String name) {
+    ConfigValue configValue = context.proceed(name);
+    if (configValue == null || configValue.getValue() == null) {
+      return null;
+    }
+    if (name.equals("quarkus.otel.sdk.disabled")
+        && configValue.getConfigSourceOrdinal() <= APPLICATION_PROPERTIES_CLASSPATH_ORDINAL
+        && configValue.getValue().equals("false")) {
+      // Check for a user-configured endpoint URL. If none is found, disable OpenTelemetry.
+      ConfigValue tracesEndpoint = context.proceed("quarkus.otel.exporter.otlp.traces.endpoint");
+      if (tracesEndpoint.getConfigSourceOrdinal() <= APPLICATION_PROPERTIES_CLASSPATH_ORDINAL) {
+        tracesEndpoint = context.proceed("quarkus.otel.exporter.otlp.endpoint");
+      }
+      if (tracesEndpoint.getConfigSourceOrdinal() <= APPLICATION_PROPERTIES_CLASSPATH_ORDINAL) {
+        LOGGER.info("No OpenTelemetry endpoint configured, disabling OpenTelemetry");
+        LOGGER.info(
+            "To enable OpenTelemetry, define a collector endpoint URL "
+                + "using the property: quarkus.otel.exporter.otlp.traces.endpoint");
+        configValue = configValue.withValue("true");
+      } else {
+        LOGGER.info(
+            "Found OpenTelemetry collector endpoint URL: {}; enabling OpenTelemetry",
+            tracesEndpoint.getValue());
+      }
+    }
+    return configValue;
+  }
+}
