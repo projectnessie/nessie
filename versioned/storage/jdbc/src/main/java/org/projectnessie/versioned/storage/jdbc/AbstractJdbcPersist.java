a16ee1e2aebc47b0c014a1d1a7372059ad2d2d0a
Alexandre Dutra <adutra@users.noreply.github.com>
2024-05-21 16:58:22 +0200

    nit

--- versioned/storage/jdbc/src/main/java/org/projectnessie/versioned/storage/jdbc/AbstractJdbcPersist.java
+++ versioned/storage/jdbc/src/main/java/org/projectnessie/versioned/storage/jdbc/AbstractJdbcPersist.java
@@ -507,7 +507,7 @@ abstract class AbstractJdbcPersist implements Persist {
             for (int i = 0; i < updated.length; i++) {
               if (updated[i] == 1) {
                 r[batchIndexToObjIndex.get(i)] = true;
-              } else if (updated[i] < 0 || updated[i] > 1) {
+              } else if (updated[i] != 0) {
                 throw new IllegalStateException(
                     "driver returned unexpected value for a batch update: " + updated[i]);
               }
