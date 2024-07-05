8fb06068ebd46806171dfd969ccec1fe40749c9d
Robert Stupp <snazy@snazy.de>
2024-07-05 12:31:46 +0200

    Update catalog/files/impl/src/main/java/org/projectnessie/catalog/files/s3/S3Config.java
    Co-authored-by: Dmitri Bourlatchkov <dmitri.bourlatchkov@gmail.com>

--- catalog/files/impl/src/main/java/org/projectnessie/catalog/files/s3/S3Config.java
+++ catalog/files/impl/src/main/java/org/projectnessie/catalog/files/s3/S3Config.java
@@ -100,7 +100,7 @@ public interface S3Config {
    * nessie.catalog.service.s3.key-store.password} must be supplied as well when providing a custom
    * key store.
    *
-   * <p>When running in k8s or Docker, the path is local withing the pod/container and must be
+   * <p>When running in k8s or Docker, the path is local within the pod/container and must be
    * explicitly mounted.
    */
   @ConfigItem(section = "transport")
