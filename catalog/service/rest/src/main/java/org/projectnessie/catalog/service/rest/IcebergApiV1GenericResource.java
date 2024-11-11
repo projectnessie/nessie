9c69df674687a1e17c3a9ef943c17d755c15a976
Robert Stupp <snazy@snazy.de>
2024-11-11 10:57:12 +0100

    Catalog/config: add endpoints to config response
    This becomes _effectively mandatory_ for Iceberg 1.7.0 clients, actually a breaking change in Iceberg 1.7.0.

--- catalog/service/rest/src/main/java/org/projectnessie/catalog/service/rest/IcebergApiV1GenericResource.java
+++ catalog/service/rest/src/main/java/org/projectnessie/catalog/service/rest/IcebergApiV1GenericResource.java
@@ -17,6 +17,7 @@ package org.projectnessie.catalog.service.rest;
 
 import static java.util.Objects.requireNonNull;
 import static org.projectnessie.catalog.formats.iceberg.nessie.CatalogOps.CATALOG_UPDATE_MULTIPLE;
+import static org.projectnessie.catalog.formats.iceberg.rest.IcebergEndpoint.icebergEndpoint;
 import static org.projectnessie.model.Content.Type.ICEBERG_TABLE;
 
 import io.smallrye.common.annotation.Blocking;
@@ -35,6 +36,7 @@ import jakarta.ws.rs.QueryParam;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import java.io.IOException;
+import java.util.List;
 import java.util.Map;
 import org.eclipse.microprofile.openapi.annotations.Operation;
 import org.jboss.resteasy.reactive.server.ServerExceptionMapper;
@@ -42,6 +44,7 @@ import org.projectnessie.api.v2.params.ParsedReference;
 import org.projectnessie.catalog.formats.iceberg.rest.IcebergCatalogOperation;
 import org.projectnessie.catalog.formats.iceberg.rest.IcebergCommitTransactionRequest;
 import org.projectnessie.catalog.formats.iceberg.rest.IcebergConfigResponse;
+import org.projectnessie.catalog.formats.iceberg.rest.IcebergEndpoint;
 import org.projectnessie.catalog.service.api.CatalogCommit;
 import org.projectnessie.catalog.service.api.SnapshotReqParams;
 import org.projectnessie.catalog.service.config.LakehouseConfig;
@@ -63,6 +66,80 @@ public class IcebergApiV1GenericResource extends IcebergApiV1ResourceBase {
 
   @Inject IcebergConfigurer icebergConfigurer;
   @Inject IcebergErrorMapper errorMapper;
+
+  public static final String V1_NAMESPACES = "/v1/{prefix}/namespaces";
+  public static final String V1_NAMESPACE = "/v1/{prefix}/namespaces/{namespace}";
+  public static final String V1_NAMESPACE_PROPERTIES =
+      "/v1/{prefix}/namespaces/{namespace}/properties";
+  public static final String V1_TABLES = "/v1/{prefix}/namespaces/{namespace}/tables";
+  public static final String V1_TABLE = "/v1/{prefix}/namespaces/{namespace}/tables/{table}";
+  public static final String V1_TABLE_REGISTER = "/v1/{prefix}/namespaces/{namespace}/register";
+  public static final String V1_TABLE_METRICS =
+      "/v1/{prefix}/namespaces/{namespace}/tables/{table}/metrics";
+  public static final String V1_TABLE_RENAME = "/v1/{prefix}/tables/rename";
+  public static final String V1_TRANSACTIONS_COMMIT = "/v1/{prefix}/transactions/commit";
+  public static final String V1_VIEWS = "/v1/{prefix}/namespaces/{namespace}/views";
+  public static final String V1_VIEW = "/v1/{prefix}/namespaces/{namespace}/views/{view}";
+  public static final String V1_VIEW_RENAME = "/v1/{prefix}/views/rename";
+
+  // namespace endpoints
+  public static final IcebergEndpoint V1_LIST_NAMESPACES = icebergEndpoint("GET", V1_NAMESPACES);
+  public static final IcebergEndpoint V1_LOAD_NAMESPACE = icebergEndpoint("GET", V1_NAMESPACE);
+  public static final IcebergEndpoint V1_CREATE_NAMESPACE = icebergEndpoint("POST", V1_NAMESPACES);
+  public static final IcebergEndpoint V1_UPDATE_NAMESPACE =
+      icebergEndpoint("POST", V1_NAMESPACE_PROPERTIES);
+  public static final IcebergEndpoint V1_DELETE_NAMESPACE = icebergEndpoint("DELETE", V1_NAMESPACE);
+  public static final IcebergEndpoint V1_COMMIT_TRANSACTION =
+      icebergEndpoint("POST", V1_TRANSACTIONS_COMMIT);
+
+  // table endpoints
+  public static final IcebergEndpoint V1_LIST_TABLES = icebergEndpoint("GET", V1_TABLES);
+  public static final IcebergEndpoint V1_LOAD_TABLE = icebergEndpoint("GET", V1_TABLE);
+  public static final IcebergEndpoint V1_CREATE_TABLE = icebergEndpoint("POST", V1_TABLES);
+  public static final IcebergEndpoint V1_UPDATE_TABLE = icebergEndpoint("POST", V1_TABLE);
+  public static final IcebergEndpoint V1_DELETE_TABLE = icebergEndpoint("DELETE", V1_TABLE);
+  public static final IcebergEndpoint V1_RENAME_TABLE = icebergEndpoint("POST", V1_TABLE_RENAME);
+  public static final IcebergEndpoint V1_REGISTER_TABLE =
+      icebergEndpoint("POST", V1_TABLE_REGISTER);
+  public static final IcebergEndpoint V1_REPORT_METRICS = icebergEndpoint("POST", V1_TABLE_METRICS);
+
+  // view endpoints
+  public static final IcebergEndpoint V1_LIST_VIEWS = icebergEndpoint("GET", V1_VIEWS);
+  public static final IcebergEndpoint V1_LOAD_VIEW = icebergEndpoint("GET", V1_VIEW);
+  public static final IcebergEndpoint V1_CREATE_VIEW = icebergEndpoint("POST", V1_VIEWS);
+  public static final IcebergEndpoint V1_UPDATE_VIEW = icebergEndpoint("POST", V1_VIEW);
+  public static final IcebergEndpoint V1_DELETE_VIEW = icebergEndpoint("DELETE", V1_VIEW);
+  public static final IcebergEndpoint V1_RENAME_VIEW = icebergEndpoint("POST", V1_VIEW_RENAME);
+
+  static final List<IcebergEndpoint> ENDPOINTS =
+      List.of(
+          // namespace endpoints
+          V1_LIST_NAMESPACES,
+          V1_LOAD_NAMESPACE,
+          V1_CREATE_NAMESPACE,
+          V1_UPDATE_NAMESPACE,
+          V1_DELETE_NAMESPACE,
+          V1_COMMIT_TRANSACTION,
+
+          // table endpoints
+          V1_LIST_TABLES,
+          V1_LOAD_TABLE,
+          V1_CREATE_TABLE,
+          V1_UPDATE_TABLE,
+          V1_DELETE_TABLE,
+          V1_RENAME_TABLE,
+          V1_REGISTER_TABLE,
+          V1_REPORT_METRICS,
+
+          // view endpoints
+          V1_LIST_VIEWS,
+          V1_LOAD_VIEW,
+          V1_CREATE_VIEW,
+          V1_UPDATE_VIEW,
+          V1_DELETE_VIEW,
+          V1_RENAME_VIEW
+          //
+          );
 
   @SuppressWarnings("unused")
   public IcebergApiV1GenericResource() {
@@ -105,6 +182,7 @@ public class IcebergApiV1GenericResource extends IcebergApiV1ResourceBase {
     IcebergConfigResponse.Builder configResponse = IcebergConfigResponse.builder();
     icebergConfigurer.icebergWarehouseConfig(
         reference, warehouse, configResponse::putDefault, configResponse::putOverride);
+    configResponse.endpoints(ENDPOINTS);
     return configResponse.build();
   }
 
