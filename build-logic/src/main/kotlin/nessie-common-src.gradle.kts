projectnessie/nessie

commit feafc965a47f9243e0403ac2afd67e0e0972424d

Build: tackle Gradle deprecations marked as for-removal in Gradle 9 (#9965)

build-logic/src/main/kotlin/nessie-common-src.gradle.kts
@@ -178,7 +178,7 @@ class MemoizedGitInfo {
   companion object {
     private fun execProc(rootProject: Project, cmd: String, vararg args: Any): String {
       val buf = ByteArrayOutputStream()
-      rootProject.exec {
+      rootProject.providers.exec {
         executable = cmd
         args(args.toList())
         standardOutput = buf

build-logic/build.gradle.kts
@@ -45,3 +45,7 @@ tasks.register("codeChecks").configure {
   description = "Runs code style and license checks"
   dependsOn("spotlessCheck")
 }
+
+kotlin {
+  jvmToolchain { (this as JavaToolchainSpec).languageVersion.set(JavaLanguageVersion.of(21)) }
+}

build-logic/src/main/kotlin/Utilities.kt
@@ -23,6 +23,7 @@ import org.gradle.api.Action
 import org.gradle.api.JavaVersion
 import org.gradle.api.Project
 import org.gradle.api.Task
+import org.gradle.api.UnknownProjectException
 import org.gradle.api.artifacts.Dependency
 import org.gradle.api.artifacts.ExternalModuleDependency
 import org.gradle.api.artifacts.ModuleDependency
@@ -214,9 +215,9 @@ fun DependencyHandlerScope.nessieProject(
   if (artifactId.startsWith(":")) {
     throw IllegalArgumentException("artifactId for nessieProject() must not start with ':'")
   }
-  return if (!isIncludedInNesQuEIT(project(":").dependencyProject.gradle)) {
+  return try {
     project(":$artifactId", configuration)
-  } else {
+  } catch (e: UnknownProjectException) {
     val groupId = NessieProjects.groupIdForArtifact(artifactId)
     create(groupId, artifactId, configuration = configuration)
   }

tools/doc-generator/site-gen/build.gradle.kts
@@ -166,99 +166,87 @@ val cliHelp by tasks.registering(JavaExec::class) {
 
 val gcHelpDir = layout.buildDirectory.dir("gcHelp")
 
-val gcHelp by tasks.registering(JavaExec::class) {
+val gcHelp by tasks.registering(Sync::class) {
+  into(gcHelpDir)
+}
 
-  inputs.files(gcRunner)
-  outputs.dir(gcHelpDir)
+for (cmdArgs in listOf(
+  listOf("help", "--help")) +
+  listOf("mark",
+    "sweep",
+    "gc",
+    "list",
+    "delete",
+    "list-deferred",
+    "deferred-deletes",
+    "show",
+    "show-sql-create-schema-script",
+    "create-sql-schema",
+    "completion-script",
+    "show-licenses").map { cmd -> listOf("help-$cmd", "help", cmd) }
+) {
+  val name = cmdArgs[0]
+  val t = tasks.register<JavaExec>("gc-$name")
+  t.configure {
+    inputs.files(gcRunner)
+    val dir = layout.buildDirectory.dir("gc-$name")
+    outputs.dir(dir)
 
-  classpath(gcRunner)
+    classpath(gcRunner)
 
-  val gcMainClass = "org.projectnessie.gc.tool.cli.CLI"
+    val gcMainClass = "org.projectnessie.gc.tool.cli.CLI"
 
-  mainClass = gcMainClass
-  args("--help")
+    mainClass = gcMainClass
+    args(cmdArgs.subList(1, cmdArgs.size))
 
-  doFirst {
-    delete(gcHelpDir)
-  }
+    standardInput = InputStream.nullInputStream()
+    standardOutput = ByteArrayOutputStream()
 
-  standardInput = InputStream.nullInputStream()
-  standardOutput = ByteArrayOutputStream()
-
-  doLast {
-    gcHelpDir.get().asFile.mkdirs()
-
-    file(gcHelpDir.get().file("gc-help.md")).writeText("```\n$standardOutput\n```\n")
-
-    for (cmd in listOf(
-      "mark",
-      "sweep",
-      "gc",
-      "list",
-      "delete",
-      "list-deferred",
-      "deferred-deletes",
-      "show",
-      "show-sql-create-schema-script",
-      "create-sql-schema",
-      "completion-script",
-      "show-licenses"
-    )) {
-      logger.info("Generating GC command help for '$cmd' ...")
-      val capture = ByteArrayOutputStream()
-      javaexec {
-        mainClass = gcMainClass
-        classpath(gcRunner)
-        standardInput = InputStream.nullInputStream()
-        standardOutput = capture
-        args("help", cmd)
-      }
-      file(gcHelpDir.get().file("gc-help-$cmd.md")).writeText("```\n$capture\n```\n")
+    doLast {
+      dir.get().asFile.mkdirs()
+      file(dir.get().file("gc-$name.md")).writeText("```\n$standardOutput\n```\n")
     }
   }
+  gcHelp.configure { from(t) }
 }
 
 val serverAdminHelpDir = layout.buildDirectory.dir("serverAdminHelp")
 
-val serverAdminHelp by tasks.registering(JavaExec::class) {
+val serverAdminHelp by tasks.registering(Sync::class) {
+  into(serverAdminHelpDir)
+}
 
-  inputs.files(serverAdminRunner)
-  outputs.dir(serverAdminHelpDir)
-
-  classpath(serverAdminRunner)
-  args("help")
-
-  doFirst {
-    delete(serverAdminHelpDir)
-  }
-
-  standardOutput = ByteArrayOutputStream()
-
-  doLast {
-    serverAdminHelpDir.get().asFile.mkdirs()
-
-    file(serverAdminHelpDir.get().file("serverAdmin-help.md")).writeText("```\n$standardOutput\n```\n")
-
-    for (cmd in listOf(
+for (cmdArgs in listOf(
+  listOf("help", "help")) +
+  listOf(
       "info",
       "check-content",
       "delete-catalog-tasks",
       "erase-repository",
       "export",
       "import",
-      "show-licenses"
-    )) {
-      logger.info("Generating GC command help for '$cmd' ...")
-      val capture = ByteArrayOutputStream()
-      javaexec {
-        standardInput = InputStream.nullInputStream()
-        standardOutput = capture
-        classpath(serverAdminRunner)
-        args("help", cmd)
-      }
-      file(serverAdminHelpDir.get().file("serverAdmin-help-$cmd.md")).writeText("```\n$capture\n```\n")
+      "show-licenses").map { cmd -> listOf("help-$cmd", "help", cmd) }
+) {
+  val name = cmdArgs[0]
+  val t = tasks.register<JavaExec>("serverAdmin-$name")
+  t.configure {
+    inputs.files(serverAdminRunner)
+    val dir = layout.buildDirectory.dir("serverAdmin-$name")
+    outputs.dir(dir)
+
+    classpath(serverAdminRunner)
+
+    args(cmdArgs.subList(1, cmdArgs.size))
+
+    standardInput = InputStream.nullInputStream()
+    standardOutput = ByteArrayOutputStream()
+
+    doLast {
+      dir.get().asFile.mkdirs()
+      file(dir.get().file("serverAdmin-$name.md")).writeText("```\n$standardOutput\n```\n")
     }
   }
+  serverAdminHelp.configure { from(t) }
 }
 
 tasks.register<Sync>("generateDocs") {

commit 77855369987a524dea6829ca63fd98d79798cf45

Build/release: Fix Git info after #9965
Getting Git information in `nessie-common-src.gradle.kts` fails after #9965 with a `java.lang.UnsupportedOperationException: Standard streams cannot be configured for exec output provider`. The fix is rather simple.

build-logic/src/main/kotlin/nessie-common-src.gradle.kts
@@ -14,8 +14,6 @@
  * limitations under the License.
  */
 
-import java.io.ByteArrayOutputStream
-import java.nio.charset.StandardCharsets
 import java.util.Properties
 import net.ltgt.gradle.errorprone.CheckSeverity
 import net.ltgt.gradle.errorprone.errorprone
@@ -177,13 +175,16 @@ if (project.hasProperty("release")) {
 class MemoizedGitInfo {
   companion object {
     private fun execProc(rootProject: Project, cmd: String, vararg args: Any): String {
-      val buf = ByteArrayOutputStream()
-      rootProject.providers.exec {
-        executable = cmd
-        args(args.toList())
-        standardOutput = buf
-      }
-      return buf.toString(StandardCharsets.UTF_8).trim()
+      var out =
+        rootProject.providers
+          .exec {
+            executable = cmd
+            args(args.toList())
+          }
+          .standardOutput
+          .asText
+          .get()
+      return out.trim()
     }
 
     fun gitInfo(rootProject: Project, attribs: Attributes) {