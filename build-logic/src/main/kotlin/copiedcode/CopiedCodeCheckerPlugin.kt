aa79316cb38bcdf0e4f391ff55aaa9ba935bcc2f
Robert Stupp <snazy@snazy.de>
2025-11-24 19:45:33 +0100

    spotlessApply

--- build-logic/src/main/kotlin/copiedcode/CopiedCodeCheckerPlugin.kt
+++ build-logic/src/main/kotlin/copiedcode/CopiedCodeCheckerPlugin.kt
@@ -156,7 +156,7 @@ abstract class CheckCopiedCodeMentionsExistTask : DefaultTask() {
       logger.error(
         """
         The following {} files mentioned in {} do not exist, fix the {} file.
-        
+
         {}
         """
           .trimIndent(),
