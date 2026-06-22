7d53bcb9b2b28306f6f282490db6539ed3240ee2
Robert Stupp <snazy@snazy.de>
2026-06-22 19:51:31 +0200

    Allow setting environment variables for the Gradle invocation (#12583)
    This allows creating a `.env` file and for example enable the Gradle configuration cache via
    ```
    GRADLE_OPTS='-Dorg.gradle.configuration-cache=true'
    ```

--- build.gradle.kts
+++ build.gradle.kts
@@ -311,6 +311,7 @@ tasks.named<Wrapper>("wrapper") {
     val insertAtLine =
       scriptLines.indexOf("# Use the maximum available, or set MAX_FD != -1 to use that value.")
     scriptLines.add(insertAtLine, "")
+    scriptLines.add(insertAtLine, $$"[ -f \"${APP_HOME}/.env\" ] && . \"${APP_HOME}/.env\"")
     scriptLines.add(insertAtLine, $$". \"${APP_HOME}/gradle/gradlew-include.sh\"")
 
     scriptFile.writeText(scriptLines.joinToString("\n"))
